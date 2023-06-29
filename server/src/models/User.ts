4580781c0f02fd14a1c669a21cab35b8bfb0c824
Matvey <morozovmatvej75@gmail.com>
2023-06-29 15:38:10 +0300

    add .pre(save) method & doPasswordsMatch method on User schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,10 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-export interface IUserSchema {
+export interface IUserSchema extends Document {
   email: string;
   password: string;
+  doPasswordsMatch: (password: string) => boolean;
 }
 
 const UserSchema = new mongoose.Schema<IUserSchema>({
@@ -22,6 +24,15 @@ const UserSchema = new mongoose.Schema<IUserSchema>({
 
 const User = mongoose.model("User", UserSchema);
 
-export default User;
+UserSchema.pre("save", async function (next) {
+  if (!this.isNew) return next();
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-// TODO: add password hashing using bcrypt
+UserSchema.methods.doPasswordsMatch = function (passwordToCheck: string) {
+  return bcrypt.compare(passwordToCheck, this.password);
+};
+
+export default User;
