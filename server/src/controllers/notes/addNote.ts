499230d1aad9dd32e7d51bab634eae54443c7a17
Matvey <empflow@tutanota.com>
2023-06-30 09:31:32 +0300

    improve formatting in addNote controller

--- server/src/controllers/notes/addNote.ts
+++ server/src/controllers/notes/addNote.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import Note from "../../models/Note";
 
 export default async function addNote(req: Request, res: Response) {
-  const userId = res.locals.jwtPayload.userId;
   const { title, content, tags } = req.body;
+  const userId = res.locals.jwtPayload.userId;
+
   const note = await Note.create({ owner: userId, title, tags, content });
   res.status(201).json(note);
 }
