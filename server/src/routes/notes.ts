5bade8cc5e6ccb31079ef8b4121c9ba36b4796b1
Matvey <empflow@tutanota.com>
2023-06-30 09:12:04 +0300

    test

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import getNotesMeta from "../controllers/notes/getNotesMeta";
 const router = express.Router();
-
 router.use("/", getNotesMeta);
 
 export default router;
