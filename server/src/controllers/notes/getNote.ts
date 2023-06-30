2014e2dec4937b413320ca354718954eadf88e53
Matvey <empflow@tutanota.com>
2023-06-30 09:50:29 +0300

    add getNote controller

---
+++ server/src/controllers/notes/getNote.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import Note from "../../models/Note";
+import { NotFoundErr } from "../../utils/errs";
+
+export default async function getNote(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { noteId } = req.params;
+  const userId = res.locals.jwtPayload.userId;
+  const note = await Note.findOne({ owner: userId, _id: noteId });
+  if (!note) throw new NotFoundErr("Note not found");
+  res.status(200).json(note);
+}
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import addNote from "../controllers/notes/addNote";
+import getNote from "../controllers/notes/getNote";
 import getNotesMeta from "../controllers/notes/getNotesMeta";
 import validateAddNote from "../middleware/notes/validateAddNote";
 const router = express.Router();
 
 router.get("/", getNotesMeta);
 router.post("/add", validateAddNote, addNote);
+
+router.route("/:noteId").get(getNote);
 
 export default router;
