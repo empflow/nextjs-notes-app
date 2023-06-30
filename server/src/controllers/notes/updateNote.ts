b6241237d219a8304048439ba882189c5240773e
Matvey <empflow@tutanota.com>
2023-06-30 10:24:33 +0300

    add updateNote controller

---
+++ server/src/controllers/notes/updateNote.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import Note from "../../models/Note";
+import { NotFoundErr } from "../../utils/errs";
+
+export default async function updateNote(req: Request, res: Response) {
+  const { noteId } = req.params;
+  const userId = res.locals.jwtPayload.userId;
+  const { title, content, isInTrash, tags } = req.body;
+  const noteUpdateMsg = await Note.updateOne(
+    { owner: userId, _id: noteId },
+    { title, content, isInTrash, tags },
+    { new: true, runValidators: true }
+  );
+  if (!noteUpdateMsg.modifiedCount) throw new NotFoundErr("Note not found");
+  res.status(200).json({ ok: true });
+}
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import addNote from "../controllers/notes/addNote";
 import getNote from "../controllers/notes/getNote";
 import getNotesMeta from "../controllers/notes/getNotesMeta";
+import updateNote from "../controllers/notes/updateNote";
 import validateAddNote from "../middleware/notes/validateAddNote";
 import validateGetNote from "../middleware/notes/validateGetNote";
 const router = express.Router();
@@ -9,6 +10,6 @@ const router = express.Router();
 router.get("/", getNotesMeta);
 router.post("/add", validateAddNote, addNote);
 
-router.route("/:noteId").get(validateGetNote, getNote);
+router.route("/:noteId").get(validateGetNote, getNote).patch(updateNote);
 
 export default router;
