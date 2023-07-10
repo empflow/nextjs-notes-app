c8211a0272a384fb04c3432c6d7165cc75cdb692
Matvey <empflow@tutanota.com>
2023-07-10 18:38:56 +0300

    send {[notes]} instead of { data: { [notes] }}

--- server/src/controllers/notes/getNotesMeta.ts
+++ server/src/controllers/notes/getNotesMeta.ts
@@ -5,6 +5,5 @@ import noteMetaProjection from "../../utils/projections/noteMeta";
 export default async function getNotesMeta(req: Request, res: Response) {
   const userId: string = res.locals.jwtPayload.userId;
   const notes = await Note.find({ owner: userId }, noteMetaProjection);
-  if (!notes.length) res.status(404).json({ data: notes });
-  res.status(200).json({ data: notes });
+  res.status(200).json(notes);
 }
