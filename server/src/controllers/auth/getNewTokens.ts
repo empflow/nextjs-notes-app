3058a894036e92fbc4cfdcf464f4de9813c9a6fe
Matvey <empflow@tutanota.com>
2023-07-12 19:45:01 +0300

    add /get-new-tokens controller

--- server/src/controllers/auth/getNewTokens.ts
+++ server/src/controllers/auth/getNewTokens.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import RefreshToken from "../../models/RefreshToken";
+import { IUser } from "../../models/User";
+import { SignUpOrInResponse } from "../../types";
+import InternalServerErr from "../../utils/errs/InternalServerErr";
 
 export default async function getNewTokens(req: Request, res: Response) {
-  res.status(200).json({ hello: true });
+  const { user }: { user: IUser } = res.locals as any;
+  const {
+    refreshToken: { id: oldRefreshTokenId },
+  }: { refreshToken: { id: string } } = req.body;
+
+  const newRefreshToken = await user.getRefreshToken();
+  const newAccessToken = user.getAccessToken();
+
+  const deletedOldRefreshToken = await RefreshToken.findByIdAndDelete(
+    oldRefreshTokenId
+  );
+  if (!deletedOldRefreshToken) {
+    throw new InternalServerErr("Could not delete old refresh token");
+  }
+
+  const savedNewRefreshToken = await RefreshToken.create(newRefreshToken.forDb);
+
+  const response: SignUpOrInResponse = {
+    accessToken: newAccessToken,
+    refreshToken: {
+      id: savedNewRefreshToken.id,
+      token: newRefreshToken.plainTextToken,
+    },
+  };
+  res.status(200).json(response);
 }
