15bba25aa778aef0e673cdded3a88a509939e4cd
Matvey <empflow@tutanota.com>
2023-07-20 19:19:54 +0300

    send test req from frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,30 @@
-import Test from "./components/test";
+"use client";
 
-export default function Home() {
+import Test from "./components/test";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export default async function Home() {
+  async function sendReq() {
+    const response = await axios.post("http://localhost:4000/auth/sign-in", {
+      email: `johndo1e1@example.com`,
+      password: "sldfjsldfkj435$$",
+    });
+    const daysIn15Mins = 0.010416;
+    Cookies.set("accessToken", response.data.accessToken, {
+      expires: daysIn15Mins,
+    });
+  }
+
   return (
     <>
       <h1>home page</h1>
-      <Test />
+      <button
+        className="border-blue-500 border px-4 py-2 rounded"
+        onClick={sendReq}
+      >
+        send request!!!
+      </button>
     </>
   );
 }
