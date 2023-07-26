76174ac50302a147d3797da221e1a7bc45bb62d9
Matvey <empflow@tutanota.com>
2023-07-26 17:22:48 +0300

    Update Footer.tsx

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,16 +1,40 @@
 import Link from "next/link";
+import ThemeSwitcher from "./ThemeSwitch";
 
 export default function Footer() {
   return (
-    <footer>
-      made by{" "}
-      <Link
-        target="_blank"
-        href="https://github.com/empflow"
-        className="underline hover:text-blue-600"
-      >
-        me
-      </Link>
+    <footer className="border-t border-gray-400 px-global sm:px-global-sm py-8">
+      <div className="max-w-global m-auto">
+        <div className="mb-5 flex justify-between">
+          <div>
+            <Link href="/" style={{ textDecoration: "none" }}>
+              Notes app
+            </Link>
+          </div>
+          <div>
+            <ThemeSwitcher />
+          </div>
+        </div>
+
+        <div className="text-gray-600 dark:text-gray-500">
+          <div className="mb-2">
+            Made by{" "}
+            <Link
+              target="_blank"
+              href="https://github.com/empflow"
+              className="underline hover:text-blue-600"
+            >
+              me
+            </Link>
+          </div>
+
+          <div>
+            Copyright © 2023 Me
+            <br />
+            Absolutely no rights reserved
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
