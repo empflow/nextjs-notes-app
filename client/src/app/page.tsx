89ea6cb506d997110de36091d2e2a961abee9333
Matvey <empflow@tutanota.com>
2023-07-26 19:05:29 +0300

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default async function Home() {
   return (
     <>
-      <h1>home page</h1>
-      <h2>hello dear visitor of my website</h2>
+      <h1 className="text-3xl font-semibold">Home page</h1>
+      <h2 className="text-xl font-medium">hello dear visitor of my website</h2>
       <p>this is my landing page</p>
     </>
   );
