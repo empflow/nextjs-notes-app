dd7dc06c09850bb8931675431c4efa8b0b3bc570
Matvey <empflow@tutanota.com>
2023-07-26 18:22:12 +0300

    use newly added colors

--- client/src/app/components/Main.tsx
+++ client/src/app/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex-grow px-global sm:px-global-sm py-8 w-full bg-main">
+    <main className="flex-grow px-global sm:px-global-sm py-8 w-full bg-l-main dark:bg-d-main">
       <div className="max-w-global m-auto">{children}</div>
     </main>
   );
