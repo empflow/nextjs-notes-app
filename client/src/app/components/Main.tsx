f81c88764f246d6bfc44c7a6c7aab9cd3c48fa88
Matvey <empflow@tutanota.com>
2023-07-26 17:50:56 +0300

    extend tailwind color scheme (dark)

--- client/src/app/components/Main.tsx
+++ client/src/app/components/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex-grow px-global sm:px-global-sm py-8 w-full">
+    <main className="flex-grow px-global sm:px-global-sm py-8 w-full bg-main">
       <div className="max-w-global m-auto">{children}</div>
     </main>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,6 +16,26 @@ module.exports = {
       maxWidth: {
         global: "768px",
       },
+      backgroundColor: {
+        "d-main": "rgb(20, 20, 20)",
+        "d-secondary": "rgb(31, 31, 31)",
+        "d-tertiary": "rgb(58, 58, 58)",
+      },
+      textColor: {
+        "d-main": "rgb(220, 220, 230)",
+        "d-secondary": "rgb(119, 119, 119)",
+      },
+      borderColor: {
+        "d-main": "rgb(58, 58, 58)",
+        "d-secondary": "rgb(75, 75, 75)",
+      },
+      colors: {
+        "light-blue": "rgb(98, 160, 230)",
+        "dark-xl-gray": "rgb(90, 90, 90)",
+        "dark-gray": "rgb(115, 115, 115)",
+        gray: "rgb(136, 136, 136)",
+        "light-gray": "rgb(169, 169, 169)",
+      },
     },
   },
 };
