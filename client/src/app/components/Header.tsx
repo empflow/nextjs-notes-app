e73ac0c75632a4126e9d637fff7755691ea168e5
Matvey <empflow@tutanota.com>
2023-07-29 19:46:00 +0300

    fix header & footer border color

--- client/src/app/components/Header.tsx
+++ client/src/app/components/Header.tsx
@@ -3,11 +3,11 @@ import NameOrSignInButton from "./NameOrSignInButton";
 
 export default function Header() {
   return (
-    <header className="px-global sm:px-global-sm py-4 sticky top-0 z-10 backdrop-blur-lg bg-l-header dark:bg-d-header border-b border-light-xl-gray dark:border-dark-xl-gray">
+    <header className="px-global sm:px-global-sm py-3 sticky top-0 z-10 backdrop-blur-lg bg-l-header dark:bg-d-header border-b border-light-xl-gray dark:border-dark-3xl-gray">
       <div className="flex max-w-global m-auto md justify-between w-full">
         <div>
-          <Link href="/">
-            <h2>Notes app</h2>
+          <Link href="/" className="no-underline">
+            <h2 className="text-white text-xl">Notes app</h2>
           </Link>
         </div>
 
