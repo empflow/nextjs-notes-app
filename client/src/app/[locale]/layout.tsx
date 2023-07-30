0c9c41019207b182219ef7f62511ed0302179ee5
Matvey <empflow@tutanota.com>
2023-07-30 19:47:25 +0300

    add support for translations in client components

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -1,10 +1,12 @@
 import "../globals.css";
+import { NextIntlClientProvider } from "next-intl";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Providers from "../providers";
 import { notFound } from "next/navigation";
 import { useLocale } from "next-intl";
+import locales from "@/config/locales";
 
 export const metadata = {
   title: "Notes",
@@ -18,21 +20,37 @@ interface RootLayoutContext {
   };
 }
 
-export default function RootLayout({ children, params }: RootLayoutContext) {
+export function generateStaticParams(): RootLayoutContext["params"][] {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: RootLayoutContext) {
   const locale = useLocale();
 
   if (params.locale !== locale) {
     notFound();
   }
 
+  let messages;
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch (err) {
+    notFound();
+  }
+
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Providers>
-          <Header />
-          <Main>{children}</Main>
-          <Footer />
-        </Providers>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Providers>
+            <Header />
+            <Main>{children}</Main>
+            <Footer />
+          </Providers>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
