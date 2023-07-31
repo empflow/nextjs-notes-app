ba80c5c7601e4f43e140f455b521437acbefd4d7
Matvey <empflow@tutanota.com>
2023-07-31 13:29:45 +0300

    add ICU syntax examples

---
+++ client/src/messages/examples.ts
@@ -0,0 +1,17 @@
+/*
+  // select
+  "message": "{gender, select, male {He} female {She} other {They} } will respond asap"
+
+  // currency
+  "message": "This costs { price, number, ::sign compact-short currency/USD }"
+  // example above doesn't work with  all currencies (only tested on RUB), so below is probably a better alternative, not sure
+  "message": "This costs ₽{ price, number }
+
+  // numbers (endings like st, nd, rd, th)
+  "message": "This is your { year, selectordinal, one {#st} two {#nd} few {#rd} other {#th} }"
+
+  // normal numbers
+  "message": "The number is {num}"
+*/
+
+export {};
