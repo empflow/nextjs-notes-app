e1d6c4c5633c51f5df62cddda25b3a12edbcf91e
Matvey <empflow@tutanota.com>
2023-08-01 14:40:14 +0300

    implement <Loading /> component

--- client/src/app/components/Loading.tsx
+++ client/src/app/components/Loading.tsx
@@ -1,6 +1,15 @@
 interface Props {
-  pxSize?: number;
+  className?: string;
 }
-export default function Loading({ pxSize }: Props) {
-  return <div>spinner...</div>;
+/**
+ *  usage: put this component inside an element with `display: relative` and set width and height manually like `width: 25px`
+ */
+export default function Loading(props: Props) {
+  const { className } = props;
+
+  return (
+    <div
+      className={`absolute top-0 bottom-0 left-0 right-0 border-4 dark:border-dark-xl-gray border-t-l-accent border-light-xl-gray dark:border-t-d-accent animate-spin rounded-full ${className}`}
+    ></div>
+  );
 }
