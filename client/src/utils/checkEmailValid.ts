fe3231158c7a08ce6c73a0845074e42e716e598e
Matvey <empflow@tutanota.com>
2023-08-01 17:02:00 +0300

    update email regex

--- client/src/utils/checkEmailValid.ts
+++ client/src/utils/checkEmailValid.ts
@@ -1,4 +1,4 @@
 export default function checkEmailValid(email: string) {
-  const regex = /^\w+@\w+\.\w+$/;
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return regex.test(email);
 }
