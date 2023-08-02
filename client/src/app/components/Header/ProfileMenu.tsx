6764e8438b5a25bee192774d598bff343c94135e
Matvey <empflow@tutanota.com>
2023-08-02 17:25:27 +0300

    add profile menu

--- client/src/app/components/Header/ProfileMenu.tsx
+++ client/src/app/components/Header/ProfileMenu.tsx
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
-import ExpandIcon from "@/icons/Expand";
+import ProfileMenuContent from "./ProfileMenuContent";
 
 export default function ProfileMenu() {
   const t = useTranslations("Header");
@@ -18,22 +18,5 @@ export default function ProfileMenu() {
     );
   }
 
-  return <Menu signedInAs={username || t("signedInAsUnknown")} />;
-}
-
-interface MenuProps {
-  signedInAs: string;
-}
-
-function Menu({ signedInAs }: MenuProps) {
-  return (
-    <div className="relative flex gap-1 items-center dark:hover:bg-dark-4xl-gray rounded  hover:bg-light-5xl-gray p-1">
-      <div className="flex text-sm font-medium items-center justify-center w-[28px] h-[28px] rounded-full border border-light-gray dark:border-gray">
-        {signedInAs[0].toUpperCase()}
-      </div>
-      <div>
-        <ExpandIcon pxSize={18} className="fill-light-gray dark:fill-gray" />
-      </div>
-    </div>
-  );
+  return <ProfileMenuContent signedInAs={username || t("signedInAsUnknown")} />;
 }
---
+++ client/src/app/components/Header/ProfileMenuContent.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import ExpandIcon from "@/icons/Expand";
+import { useState } from "react";
+import { useTranslations } from "next-intl";
+import SignOutIcon from "@/icons/SignOut";
+
+interface MenuProps {
+  signedInAs: string;
+}
+
+export default function ProfileMenuContent({ signedInAs }: MenuProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const t = useTranslations("Header");
+  const btnsT = useTranslations("Header.Buttons");
+
+  return (
+    <div className="sm:relative">
+      <div
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex items-center gap-1 rounded p-1 hover:cursor-pointer hover:bg-light-5xl-gray dark:hover:bg-dark-4xl-gray"
+        title={isOpen ? t("closeMenu") : t("openMenu")}
+      >
+        <div className="flex h-[28px] w-[28px] items-center justify-center rounded-full border border-light-gray text-sm font-medium dark:border-gray">
+          {signedInAs[0].toUpperCase()}
+        </div>
+        <div>
+          <ExpandIcon pxSize={18} className="fill-light-gray dark:fill-gray" />
+        </div>
+      </div>
+
+      {/* menu */}
+      <div
+        className={`absolute left-0 right-0 top-[70px] bg-transparent px-global dark:shadow-none  sm:left-auto sm:top-[56px] sm:px-global-sm sm:w-[300px]${
+          !isOpen ? " hidden" : ""
+        }`}
+      >
+        <div className="flex flex-col gap-3 rounded border-light-2xl-gray bg-l-secondary px-3 py-5 shadow-md dark:border-dark-3xl-gray dark:bg-d-secondary">
+          <div className="px-2 font-medium">{signedInAs}</div>
+
+          {/* list of menu buttons */}
+          <div className={`flex flex-col gap-1`}>
+            <button className="flex items-center gap-1 p-2 hover:bg-light-5xl-gray dark:hover:bg-dark-4xl-gray">
+              <SignOutIcon
+                pxSize={22}
+                className="fill-l-accent dark:fill-d-accent"
+              />
+              <div>{btnsT("signOut")}</div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
