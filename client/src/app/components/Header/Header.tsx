184f1bccd2bb2be2e0d11b65f2f5bc35c0455a2e
Matvey <empflow@tutanota.com>
2023-08-02 15:50:48 +0300

    center items vertically in <Header />

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -5,9 +5,9 @@ import ProfileMenu from "./ProfileMenu";
 export default function Header() {
   return (
     <header className="px-global sm:px-global-sm py-3 sticky top-0 z-10 backdrop-blur-lg bg-l-header dark:bg-d-header border-b border-light-xl-gray dark:border-dark-3xl-gray">
-      <div className="flex max-w-global m-auto md justify-between w-full">
+      <div className="flex max-w-global m-auto md justify-between items-center w-full">
         <div>
-          <Link href="/" className="no-underline">
+          <Link href="/">
             <Logo />
           </Link>
         </div>
