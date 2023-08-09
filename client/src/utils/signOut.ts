b5bb4bcb6c7129643c3e232d68a1a2527ff48253
Matvey <empflow@tutanota.com>
2023-08-09 17:49:40 +0300

    fix: remove duplicate/unused util function

--- client/src/utils/signOut.ts
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-import { redirect } from "next/navigation";
-
-export default function signOut() {
-  localStorage.removeItem("username");
-  Cookies.remove("accessToken");
-  Cookies.remove("refreshToken");
-  redirect("/sign-in");
-}
