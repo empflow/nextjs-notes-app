077b32ef5e21626dac3bd2a1d63d8070622cbb9e
Matvey <empflow@tutanota.com>
2023-08-10 10:28:14 +0300

    feat(server): update errHandler to optionally send errCode

--- server/src/middleware/errHandler.ts
+++ server/src/middleware/errHandler.ts
@@ -1,14 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import ApiErr, { HttpCodes } from "../utils/errs/ApiErr";
+import ApiErr, { HttpCode } from "../utils/errs/ApiErr";
 import { TApiErrs } from "../utils/errs";
 import { MongoError } from "mongodb";
 import isErrCausedByUser from "../utils/isErrCausedByUser";
+import ErrCode from "../utils/errCodes";
 
 export type TErr = TApiErrs | Error | MongoError | SyntaxError;
 
 interface IErrObject {
   message: string;
-  code?: number;
+  errCode?: ErrCode;
+  httpCode?: number;
   duplicates?: string[];
 }
 
@@ -19,18 +21,24 @@ export default function errHandler(
   next: NextFunction
 ) {
   const errObj: IErrObject = {
-    message: err.message || "No error message was provided",
+    message: err.message || "No error message provided",
   };
-  let code = 500;
 
-  if (err.name === "ApiErr") {
-    code = (err as ApiErr).code;
+  let httpCode = 500;
+  const isApiErr = err instanceof ApiErr;
+
+  if (isApiErr) {
+    if (err.errCode) errObj.errCode = err.errCode;
+  }
+
+  if (err instanceof ApiErr) {
+    httpCode = err.httpCode;
   } else if (isErrCausedByUser(err)) {
-    code = HttpCodes.BadRequest;
+    httpCode = HttpCode.BadRequest;
   } else {
     console.error(err);
     errObj.message = "Internal server error";
   }
 
-  res.status(code).json(errObj);
+  res.status(httpCode).json(errObj);
 }
