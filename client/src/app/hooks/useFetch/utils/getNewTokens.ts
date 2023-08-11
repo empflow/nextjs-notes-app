c51835f2afe804a686397f99ff2b1ec15a063871
Matvey <empflow@tutanota.com>
2023-08-11 14:13:15 +0300

    feat(client): add getNewTokens() util

---
+++ client/src/app/hooks/useFetch/utils/getNewTokens.ts
@@ -0,0 +1,42 @@
+import notify from "@/utils/notify";
+import { TTranslations } from "@/utils/types";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Formats, TranslationValues } from "next-intl";
+import React from "react";
+import hasRefreshTokenExpired from "./hasRefreshTokenExpired";
+import isErrUnknown from "./isErrUnknown";
+
+interface TGetNewTokensReturnValue {
+  err?: {
+    msg: string | React.ReactNode;
+  };
+  data?: any;
+}
+
+export default async function getNewTokens(
+  translations: TTranslations,
+  notSignedInMsg: React.ReactNode,
+): Promise<TGetNewTokensReturnValue> {
+  const refreshToken = JSON.parse(Cookies.get("refreshToken") ?? "");
+  const isOnline = navigator.onLine;
+
+  const genericErr: TGetNewTokensReturnValue = {
+    err: { msg: translations("generic") },
+  };
+
+  if (!isOnline) return { err: { msg: translations("noInternet") } };
+
+  try {
+    const resp = await axios.post("/auth/get-new-tokens", {
+      refreshToken,
+    });
+    return { data: resp.data };
+  } catch (err) {
+    if (isErrUnknown(err)) return genericErr;
+
+    const { data } = (err as any).response;
+    if (hasRefreshTokenExpired(data)) return { err: { msg: notSignedInMsg } };
+    return genericErr;
+  }
+}
