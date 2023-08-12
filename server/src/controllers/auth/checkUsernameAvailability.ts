3aded4d3f667fb889ac538d062b2d3861bce83dc
Matvey <empflow@tutanota.com>
2023-08-12 19:05:36 +0300

    feat(server): wait 1 sec before sending username availability status in dev mode

--- server/src/controllers/auth/checkUsernameAvailability.ts
+++ server/src/controllers/auth/checkUsernameAvailability.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../../models/User";
 import { BadRequestErr } from "../../utils/errs";
+import wait from "../../utils/wait";
 
 export default async function checkUsernameAvailability(
   req: Request,
@@ -10,9 +11,17 @@ export default async function checkUsernameAvailability(
   if (!username) throw new BadRequestErr("No username provided");
   const user = await User.findOne({ email: username });
 
+  await waitIfInDevMode();
+
   if (!user) {
     return res.json({ ok: true });
   }
 
   return res.status(409).json({ ok: false });
+}
+
+async function waitIfInDevMode() {
+  if (process.env.NODE_ENV === "dev") {
+    await wait(1000);
+  }
 }
