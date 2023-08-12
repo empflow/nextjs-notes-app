c6dfc4fc116800759a869f2a8b25f01ad48c69f6
Matvey <empflow@tutanota.com>
2023-08-12 19:01:37 +0300

    wip(client): add <LeftSide /> & <RightSide />

--- client/src/app/[locale]/(notes)/notes/page.tsx
+++ client/src/app/[locale]/(notes)/notes/page.tsx
@@ -2,24 +2,24 @@
 import useFetch from "@/app/hooks/useFetch/useFetch";
 import redirToSignInIfNoToken from "@/utils/redirToSignInIfNoToken";
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+import LeftSide from "./components/LeftSide";
+import RightSide from "./components/RightSide";
 
 export default function Notes() {
   const t = useTranslations("Index");
   const { data, err, fetch, loading, setLoading } = useFetch("/notes", {
     method: "get",
   });
+
+  useEffect(() => {
+    redirToSignInIfNoToken({ mode: "client" });
+  }, []);
+
   return (
-    <div>
-      <div>{err && JSON.stringify(err)}</div>
-      <div>Loading: {loading}</div>
-      <div>Data: {JSON.stringify(data)}</div>
-      <button
-        className="rounded border border-gray bg-white px-2 py-1"
-        onClick={fetch}
-      >
-        {t("title")}
-      </button>
-      <div className="w-full border border-black">hello</div>
+    <div className="flex">
+      <LeftSide />
+      <RightSide />
     </div>
   );
 }
