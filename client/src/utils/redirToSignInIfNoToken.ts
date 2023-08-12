43d881d655451ced23b87d901fae7765c74a72d2
Matvey <empflow@tutanota.com>
2023-08-12 19:01:14 +0300

    feat(client): add ability to choose client or server mode in redirToSignInIfNotToken

--- client/src/utils/redirToSignInIfNoToken.ts
+++ client/src/utils/redirToSignInIfNoToken.ts
@@ -1,10 +1,26 @@
+import Cookies from "js-cookie";
 import { cookies } from "next/dist/client/components/headers";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { redirect } from "next/navigation";
 
-export default function redirToSignInIfNoToken() {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken");
-  const refreshToken = cookieStore.get("refreshToken");
+interface Opts {
+  mode?: "client" | "server";
+}
+
+export default function redirToSignInIfNoToken(opts?: Opts) {
+  const { mode = "server" } = opts ?? {};
+
+  let accessToken: RequestCookie | string | undefined;
+  let refreshToken: RequestCookie | string | undefined;
+
+  if (mode === "server") {
+    const cookieStore = cookies();
+    refreshToken = cookieStore.get("refreshToken");
+    accessToken = cookieStore.get("accessToken");
+  } else {
+    accessToken = Cookies.get("accessToken");
+    refreshToken = Cookies.get("refreshToken");
+  }
 
   if (!refreshToken) {
     return redirect("/sign-in");
