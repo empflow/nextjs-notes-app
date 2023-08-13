eb6c02dc6e05e28721a2e481dabc25facd634aba
Matvey <empflow@tutanota.com>
2023-08-13 17:05:48 +0300

    wip(client): add state and fetched data for future use

--- client/src/app/[locale]/(notes)/notes/page.tsx
+++ client/src/app/[locale]/(notes)/notes/page.tsx
@@ -1,15 +1,50 @@
 "use client";
 import useFetch from "@/app/hooks/useFetch/useFetch";
 import redirToSignInIfNoToken from "@/utils/redirToSignInIfNoToken";
+import { TNote, TTag } from "@/utils/types";
 import { useTranslations } from "next-intl";
-import { useEffect } from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import LeftSide from "./components/LeftSide";
 import RightSide from "./components/RightSide";
 
+interface TNotesContextValue {
+  notes: TNote[] | null;
+  setNotes: Dispatch<SetStateAction<null | TNote[]>>;
+  tags: TTag[] | null;
+  setTags: Dispatch<SetStateAction<null | TTag[]>>;
+}
+
+type TNotesContext = null | TNotesContextValue;
+
+const NotesContext = createContext<TNotesContext>(null);
+
 export default function Notes() {
   const t = useTranslations("Index");
-  const { data, err, fetch, loading, setLoading } = useFetch("/notes", {
+  const [selectedNoteId, setSelectedNoteId] = useState<string | null>(null);
+  const {
+    data: notes,
+    setData: setNotes,
+    err: notesErr,
+    fetch: fetchNotes,
+    loading: notesLoading,
+  } = useFetch<TNote[]>("/notes", {
     method: "get",
+    fetchImmediately: true,
+  });
+  const {
+    data: tags,
+    setData: setTags,
+    err: tagsErr,
+    fetch: tagsFetch,
+  } = useFetch<TTag[]>("/tags", {
+    method: "get",
+    fetchImmediately: true,
   });
 
   useEffect(() => {
@@ -18,8 +53,10 @@ export default function Notes() {
 
   return (
     <div className="flex min-h-[100dvh]">
-      <LeftSide />
-      <RightSide />
+      <NotesContext.Provider value={{ notes: notes, setNotes, tags, setTags }}>
+        <LeftSide />
+        <RightSide />
+      </NotesContext.Provider>
     </div>
   );
 }
