5b798863921e339a1740074a5750d927e15a12ff
Matvey <empflow@tutanota.com>
2023-08-13 14:27:47 +0300

    feat(server): add updateTagCheckTagId mw

---
+++ server/src/middleware/tags/updateTag/checkTagId.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { isValidObjectId } from "mongoose";
+import { BadRequestErr } from "../../../utils/errs";
+
+export default async function updateTagCheckTagId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { tagId } = req.params;
+
+  if (!isValidObjectId(tagId)) {
+    throw new BadRequestErr("Invalid tag id");
+  }
+
+  next();
+}
