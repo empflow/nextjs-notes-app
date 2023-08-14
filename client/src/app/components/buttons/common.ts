d73fd1994b2bd5445bf9b8f99f10808de9258cfc
Matvey <empflow@tutanota.com>
2023-08-14 13:49:37 +0300

    feat(client): add more sharedClassNames for btns

--- client/src/app/components/buttons/common.ts
+++ client/src/app/components/buttons/common.ts
@@ -1,2 +1,2 @@
 export const baseClassName =
-  "hover:bg-blue-700 flex items-center rounded bg-l-accent text-white duration-200 dark:bg-d-accent";
+  "hover:bg-blue-700 flex items-center justify-center rounded bg-l-accent text-white duration-200 dark:bg-d-accent";
