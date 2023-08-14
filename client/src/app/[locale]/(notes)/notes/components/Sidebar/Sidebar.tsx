7e6f9987fa0f5f5a842f091f835f59a884b607ad
Matvey <empflow@tutanota.com>
2023-08-14 13:09:54 +0300

    feat(client): add <SidebarTop /> to <Sidebar />

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,21 @@
+import { TTag, TTranslations } from "@/utils/types";
+import { useTranslations } from "next-intl";
+import { useContext } from "react";
+import { NotesContext } from "../../page";
+import BigBtn from "@/app/components/buttons/Big";
+import Image from "next/image";
+import { SVGProps } from "react";
+import SidebarTop from "./Top";
+
 export default function Sidebar() {
+  const t = useTranslations("Notes");
+  const context = useContext(NotesContext);
+  if (!context) throw new Error("no context");
+  const { selectedTagId, tags } = context;
+
   return (
-    <aside className="h-full w-[350px] border-r border-light-3.5xl-gray">
-      lsdfj
+    <aside className="flex h-full w-[350px] flex-col gap-[25px] border-r border-light-3.5xl-gray p-5 dark:border-dark-4xl-gray">
+      <SidebarTop />
     </aside>
   );
 }
