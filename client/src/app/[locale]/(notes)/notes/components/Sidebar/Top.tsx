2cbe376d51fcdac752ee2faf246730e054917c86
Matvey <empflow@tutanota.com>
2023-08-14 13:09:35 +0300

    feat(client): add <SidebarTop />

---
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Top.tsx
@@ -0,0 +1,52 @@
+import BigBtn from "@/app/components/buttons/Big";
+import FilterIcon from "@/icons/svg/filter.svg";
+import AddIcon from "@/icons/svg/addCircle.svg";
+import { useContext } from "react";
+import { NotesContext } from "../../page";
+import { useTranslations } from "next-intl";
+import { TTag, TTranslations } from "@/utils/types";
+import SmallBtn from "@/app/components/buttons/Small";
+
+export default function SidebarTop() {
+  const t = useTranslations("Notes");
+  const context = useContext(NotesContext);
+  if (!context) throw new Error("no context");
+  const { selectedTagId, tags } = context;
+
+  const tagName = getTagName(tags, selectedTagId, t);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <h1 className="overflow-hidden text-ellipsis whitespace-nowrap text-[1.75rem] font-semibold text-l-main dark:text-d-main">
+          {tagName}
+        </h1>
+        <BigBtn style={{ padding: "8px" }}>
+          <AddIcon fill="white" />
+        </BigBtn>
+      </div>
+      <div className="flex justify-between">
+        <SmallBtn className="flex gap-1">
+          {t("filter")}
+          <FilterIcon fill="white" width={20} />
+        </SmallBtn>
+      </div>
+    </div>
+  );
+}
+
+function getTagName(
+  tags: TTag[] | null,
+  selectedTagId: string | null,
+  t: TTranslations,
+) {
+  const allNotesMsg = t("allNotes");
+
+  if (!tags) return allNotesMsg;
+
+  if (selectedTagId) {
+    const result = tags.find((tag) => tag._id === selectedTagId);
+    return result?.name ?? allNotesMsg;
+  }
+  return allNotesMsg;
+}
