3f441a699b6d84988a9374589d3437b9446d196b
Matvey <empflow@tutanota.com>
2023-08-14 14:38:39 +0300

    feat(client): add <SidebarNotesList /> to <Sidebar />

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,22 @@
 import { TTag, TTranslations } from "@/utils/types";
 import { useTranslations } from "next-intl";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NotesContext } from "../../page";
 import BigBtn from "@/app/components/buttons/Big";
 import Image from "next/image";
 import { SVGProps } from "react";
 import SidebarTop from "./Top";
+import SidebarNotesList from "./NotesList";
 import useGetContext from "@/app/hooks/useGetContext/useGetContext";
 
 export default function Sidebar() {
   const t = useTranslations("Notes");
   const context = useGetContext(NotesContext);
+
   return (
     <aside className="flex h-full w-[350px] flex-col gap-[25px] border-r border-light-3.5xl-gray p-5 dark:border-dark-4xl-gray">
       <SidebarTop />
+      <SidebarNotesList />
     </aside>
   );
 }
