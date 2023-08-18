37a476817d6ae310af4f913fc924b34d5934d0a3
Matvey <empflow@tutanota.com>
2023-08-18 15:37:46 +0300

    feat(client): add <EditBtn />
    bruh should've commited this days ago

---
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/NotesList/EditBtn.tsx
@@ -0,0 +1,31 @@
+import TinyBtn from "@/app/components/buttons/Tiny";
+import EditIcon from "@/icons/svg/edit.svg";
+import EditOffIcon from "@/icons/svg/editOff.svg";
+import useGetContext from "@/app/hooks/useGetContext/useGetContext";
+import { useTranslations } from "next-intl";
+import { NotesContext } from "../../../page";
+
+export default function EditBtn() {
+  const t = useTranslations("Notes");
+  const context = useGetContext(NotesContext);
+  const { isEditing, setIsEditing } = context;
+
+  return (
+    <TinyBtn
+      className="flex gap-1 duration-200"
+      onClick={() => setIsEditing((prev) => !prev)}
+    >
+      {isEditing ? (
+        <>
+          <EditOffIcon width={20} fill="white" />
+          {t("stopEditing")}
+        </>
+      ) : (
+        <>
+          <EditIcon width={20} fill="white" />
+          {t("edit")}
+        </>
+      )}
+    </TinyBtn>
+  );
+}
