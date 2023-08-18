ad13ad1e0754877cdbfe90fee54bd87d4e616a83
Matvey <empflow@tutanota.com>
2023-08-18 15:26:05 +0300

    feat(client): make storeAuthData() type-safe

--- client/src/utils/storeAuthRespData.ts
+++ client/src/utils/storeAuthRespData.ts
@@ -1,27 +1,31 @@
-import { daysIn15Mins, ninetyDays } from "@/constants/time";
-import axios from "@/config/axios";
-import Cookies from "js-cookie";
-import notify from "./notify";
-import { TTranslations } from "./types";
+import Cookies, { CookieAttributes } from "js-cookie";
+import { TAuthResp } from "@shared/types";
+
+const ninetyDays = 90;
+const daysIn15Mins = 0.010416;
+
+const commonOpts: CookieAttributes = {
+  sameSite: "Strict",
+};
 
 /**
  * this is used to store data received from the backend on sign in or sign up
  * @param data data from /auth/sign-up or /auth/sign-in (url may change, but this is unlikely)
  */
-export default function storeAuthRespData(
-  data: any,
-  errTranslations: TTranslations,
-) {
+export default function storeAuthRespData(data: TAuthResp) {
   const { refreshToken, accessToken, username } = data;
 
-  if (!refreshToken || !accessToken || !username) {
-    return notify(errTranslations("generic"));
-  }
-  const refreshTokenSerialized = JSON.stringify(data.refreshToken);
+  const refreshTokenSerialized = JSON.stringify(refreshToken);
 
   Cookies.set("accessToken", accessToken, {
     expires: daysIn15Mins,
+    ...commonOpts,
   });
-  Cookies.set("refreshToken", refreshTokenSerialized, { expires: ninetyDays });
-  Cookies.set("username", username);
+  Cookies.set("refreshToken", refreshTokenSerialized, {
+    expires: ninetyDays,
+    ...commonOpts,
+  });
+  if (username) {
+    Cookies.set("username", username, { ...commonOpts });
+  }
 }
