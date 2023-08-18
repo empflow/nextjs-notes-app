55131cb67e0748ae0aa2cde1b572e0855a0f41ff
Matvey <empflow@tutanota.com>
2023-08-18 19:15:06 +0300

    feat(client): properly render notes in <NotesList />

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/NotesList/NotesList.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/NotesList/NotesList.tsx
@@ -3,45 +3,50 @@ import MediumBtn from "@/app/components/buttons/Medium";
 import SmallBtn from "@/app/components/buttons/Small";
 import TinyBtn from "@/app/components/buttons/Tiny";
 import useGetContext from "@/app/hooks/useGetContext/useGetContext";
-import EditIcon from "@/icons/svg/edit.svg";
-import EditOffIcon from "@/icons/svg/editOff.svg";
+
 import { TWidth } from "@/utils/types";
 import { useTranslations } from "next-intl";
 import { useContext, useEffect, useRef, useState } from "react";
 import { NotesContext } from "../../../page";
+import EditBtn from "./EditBtn";
+import Note from "./Note";
 
 export default function SidebarNotesList() {
   const context = useGetContext(NotesContext);
+  const { notes, selectedNoteId, setSelectedNoteId } = context;
+
+  const notesElems =
+    notes?.map((note, i, notes) => {
+      const { _id, title } = note;
+      // TODO: add proper descriptions
+      // const desc = getNoteDesc(content);
+      const desc = `description ${i}`;
+
+      const isSelected = selectedNoteId === _id;
+      const nextNote = notes[i + 1];
+      const isAboveSelectedNote = nextNote && selectedNoteId === nextNote._id;
+      const props = {
+        title,
+        desc,
+        isSelected,
+        setSelectedNoteId,
+        isAboveSelectedNote,
+        _id,
+      };
+      return <Note {...props} key={i} />;
+    }) ?? null;
+
   return (
     <div className="flex flex-col gap-3">
       <div>
         <EditBtn />
       </div>
+      <div>{notes && notesElems}</div>
     </div>
   );
 }
 
-function EditBtn() {
-  const t = useTranslations("Notes");
-  const context = useGetContext(NotesContext);
-  const { isEditing, setIsEditing } = context;
-
-  return (
-    <TinyBtn
-      className="flex gap-1 duration-200"
-      onClick={() => setIsEditing((prev) => !prev)}
-    >
-      {isEditing ? (
-        <>
-          <EditOffIcon width={20} fill="white" />
-          {t("stopEditing")}
-        </>
-      ) : (
-        <>
-          <EditIcon width={20} fill="white" />
-          {t("edit")}
-        </>
-      )}
-    </TinyBtn>
-  );
+function getNoteDesc(noteContent: string | unknown) {
+  if (!noteContent || typeof noteContent !== "string") return "";
+  return noteContent.trim().split("\n")[1];
 }
