61a248bf79b5b70c5beaa95282b0966d510e0a30
Matvey <empflow@tutanota.com>
2023-08-21 15:59:26 +0300

    fix(client): fix client & server rendered html mismatch

--- client/src/utils/protectedPage.ts
+++ client/src/utils/protectedPage.ts
@@ -1,23 +1,9 @@
-import Cookies from "js-cookie";
 import { cookies } from "next/dist/client/components/headers";
-import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { redirect } from "next/navigation";
 
-interface Params {
-  mode: "client" | "server";
-}
-
-export default function protectedPage(opts: Params) {
-  const { mode = "server" } = opts ?? {};
-
-  let refreshToken: RequestCookie | string | undefined;
-
-  if (mode === "server") {
-    const cookieStore = cookies();
-    refreshToken = cookieStore.get("refreshToken");
-  } else {
-    refreshToken = Cookies.get("refreshToken");
-  }
+export default function protectedPage() {
+  const cookieStore = cookies();
+  const refreshToken = cookieStore.get("refreshToken");
 
   if (!refreshToken) {
     return redirect("/sign-in");
