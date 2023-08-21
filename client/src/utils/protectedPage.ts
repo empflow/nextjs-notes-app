72d5bbb288ffffcd49be956e9f813551c40116d3
Matvey <empflow@tutanota.com>
2023-08-21 13:19:57 +0300

    refactor(client): rename util function

--- client/src/utils/protectedPage.ts
+++ client/src/utils/protectedPage.ts
@@ -3,22 +3,19 @@ import { cookies } from "next/dist/client/components/headers";
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { redirect } from "next/navigation";
 
-interface Opts {
-  mode?: "client" | "server";
+interface Params {
+  mode: "client" | "server";
 }
 
-export default function redirToSignInIfNoToken(opts?: Opts) {
+export default function protectedPage(opts: Params) {
   const { mode = "server" } = opts ?? {};
 
-  let accessToken: RequestCookie | string | undefined;
   let refreshToken: RequestCookie | string | undefined;
 
   if (mode === "server") {
     const cookieStore = cookies();
     refreshToken = cookieStore.get("refreshToken");
-    accessToken = cookieStore.get("accessToken");
   } else {
-    accessToken = Cookies.get("accessToken");
     refreshToken = Cookies.get("refreshToken");
   }
 
