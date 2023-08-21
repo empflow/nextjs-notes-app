805ae8d5f9b8da1b34a01706bad66e23bb09b363
Matvey <empflow@tutanota.com>
2023-08-21 15:58:10 +0300

    fix(client): fix /notes sidebar height

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Controls from "./Controls/Controls";
 
 export default function Sidebar() {
   return (
-    <aside className="flex h-full w-[350px] flex-col gap-2 border-r border-light-3.5xl-gray p-5 dark:border-dark-4xl-gray">
+    <aside className="flex min-h-[100dvh] w-[350px] flex-col gap-2 border-r border-light-3.5xl-gray p-5 dark:border-dark-4xl-gray">
       <Controls />
       <NotesList />
     </aside>
