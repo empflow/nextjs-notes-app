bceb33ac9ecdd71fd69e078056ca598b630acb5c
Matvey <empflow@tutanota.com>
2023-08-29 11:00:36 +0300

    feat: improve <Loading /> styling

--- client/src/app/components/Loading.tsx
+++ client/src/app/components/Loading.tsx
@@ -5,21 +5,17 @@ interface Props {
   childStyle?: CSSProperties;
   pxSize?: number;
 }
-/**
- *  usage: put this component inside an element with `display: relative` and set width and height
- *  also you most likely need to use `display: flex; align-items: center; justify-content: center;` to center the spinner
- */
 export default function Loading(props: Props) {
   const { pxSize = 25, childStyle, parentStyle } = props;
 
   return (
     <div
-      className={`relative flex justify-center items-center`}
+      className={`relative flex items-center justify-center`}
       style={{ ...parentStyle, width: pxSize, height: pxSize }}
     >
       <div
         style={childStyle}
-        className={`absolute top-0 bottom-0 left-0 right-0 border-4 dark:border-dark-xl-gray border-t-l-accent border-light-xl-gray dark:border-t-d-accent animate-spin rounded-full`}
+        className={`absolute bottom-0 left-0 right-0 top-0 animate-spin rounded-full border-4 border-light-xl-gray border-t-l-accent dark:border-dark-xl-gray dark:border-t-d-accent`}
       ></div>
     </div>
   );
