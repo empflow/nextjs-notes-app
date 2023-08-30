021164ceb1db215a5c1adc9128062df90e401ce5
Matvey <empflow@tutanota.com>
2023-08-30 16:55:43 +0300

    feat: add useSignInQuery()
    forgot to commit before dammit

---
+++ client/src/app/hooks/queries/useSignInQuery.ts
@@ -0,0 +1,36 @@
+import { TSignInFormInputValues } from "@/app/[locale]/(main)/auth/sign-in/components/SignInForm";
+import http from "@/utils/http";
+import { getCaptchaTokenOrBypassToken } from "@/utils/getCaptchaTokenOrBypassToken";
+import { authRespSchema, TAuthResp } from "@shared/types";
+import { useQuery } from "@tanstack/react-query";
+import { RefObject } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+interface TProps {
+  formData: TSignInFormInputValues;
+  captchaRef: RefObject<ReCAPTCHA>;
+}
+
+export default function useSignInQuery({ formData, captchaRef }: TProps) {
+  async function fetchSignIn() {
+    const objWithCaptchaTokenOrBypassToken = await getCaptchaTokenOrBypassToken(
+      captchaRef,
+    );
+    const payload = {
+      ...objWithCaptchaTokenOrBypassToken,
+      ...formData,
+    };
+
+    const { data } = await http.post("/auth/sign-in", payload);
+
+    const signInRespData = authRespSchema.parse(data);
+    return signInRespData;
+  }
+
+  const query = useQuery<TAuthResp>(["signIn"], fetchSignIn, {
+    enabled: false,
+    retry: false,
+  });
+
+  return query;
+}
