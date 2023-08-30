14984a7cdbd1785f9318a5b9520ec01ee1a9d115
Matvey <empflow@tutanota.com>
2023-08-30 16:53:47 +0300

    fix: don't show sign-in in profile menu link if there's no access token

--- client/src/app/components/Header/ProfileMenu/ProfileMenu.tsx
+++ client/src/app/components/Header/ProfileMenu/ProfileMenu.tsx
@@ -7,15 +7,14 @@ export default function ProfileMenu() {
   const t = useTranslations("Header");
   const cookiesStore = cookies();
   const username = cookiesStore.get("username")?.value ?? null;
-  const accessToken = cookiesStore.get("accessToken")?.value ?? null;
   const refreshToken = cookiesStore.get("refreshToken")?.value ?? null;
 
-  if (!username || !accessToken || !refreshToken) {
     return (
       <Link href="/sign-in">
         <button>Sign in</button>
       </Link>
     );
+  if (!username || !refreshToken) {
   }
 
   return <ProfileMenuContent signedInAs={username || t("signedInAsUnknown")} />;
