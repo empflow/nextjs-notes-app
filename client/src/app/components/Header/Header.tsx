9243a9233a6818826f867a74c1b52bc399e3bfda
Matvey <empflow@tutanota.com>
2023-08-30 16:52:17 +0300

    refactor <Header />
    removed unnecessary divs

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -6,15 +6,11 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-10 border-b border-light-xl-gray bg-l-header px-global py-3 backdrop-blur-lg dark:border-dark-3xl-gray dark:bg-d-header sm:px-global-sm">
       <div className="md m-auto flex w-full max-w-global items-center justify-between">
-        <div>
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
+        <Link href="/">
+          <Logo />
+        </Link>
 
-        <div>
-          <ProfileMenu />
-        </div>
+        <ProfileMenu />
       </div>
     </header>
   );
