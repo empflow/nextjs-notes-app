d13a8c8ca4bd14db3b40f55bd83c215e1e0656b8
Matvey <empflow@tutanota.com>
2023-08-30 16:37:07 +0300

    feat: add <DontHaveAccount />

---
+++ client/src/app/[locale]/(main)/auth/sign-in/components/DontHaveAccount.tsx
@@ -0,0 +1,15 @@
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+
+export default function DontHaveAccount() {
+  const t = useTranslations("SignIn");
+
+  return (
+    <p>
+      {t("dontHaveAccount")}{" "}
+      <Link className="text-l-accent dark:text-d-accent" href="/auth/sign-up">
+        {t("dontHaveAccountSignUpLink")}
+      </Link>
+    </p>
+  );
+}
