51471063f8769a988324021562523986ae381540
Matvey <empflow@tutanota.com>
2023-09-01 14:36:25 +0300

    feat(client): add <Emailinput /> for <SignInForm />

---
+++ client/src/app/[locale]/(main)/auth/sign-in/components/EmailInput.tsx
@@ -0,0 +1,25 @@
+import Input from "@/app/components/form/Input";
+import useGetContext from "@/app/hooks/useGetContext";
+import SignInFormContext from "@/contexts/SignInFormContext";
+import { emailRegex } from "@shared/regexes";
+import useSignInTranslations from "./hooks/useFormTranslations";
+
+export default function SignInEmailInput() {
+  const { formT } = useSignInTranslations();
+  const { register, formErrs } = useGetContext(SignInFormContext);
+
+  return (
+    <Input
+      label={formT("email")}
+      register={register("email", {
+        required: formT("noEmail"),
+        pattern: {
+          value: emailRegex,
+          message: formT("invalidEmail"),
+        },
+      })}
+      type="email"
+      errMsg={formErrs.email?.message}
+    />
+  );
+}
