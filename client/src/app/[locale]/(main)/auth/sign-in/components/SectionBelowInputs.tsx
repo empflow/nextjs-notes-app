2d7e9df1d244d38e38ccc4d44485c1894eb0ec9e
Matvey <empflow@tutanota.com>
2023-09-01 14:35:38 +0300

    feat(client): add <SectionBelowInputs /> for <SignInForm />

---
+++ client/src/app/[locale]/(main)/auth/sign-in/components/SectionBelowInputs.tsx
@@ -0,0 +1,19 @@
+import FormBtn from "@/app/components/form/FormBtn";
+import FormErr from "@/app/components/form/FormErr";
+import useGetContext from "@/app/hooks/useGetContext";
+import SignInFormContext from "@/contexts/SignInFormContext";
+import SignInDontHaveAccount from "./DontHaveAccount";
+import useSignInTranslations from "./hooks/useFormTranslations";
+
+export default function SignInSectionBelowInputs() {
+  const { formErrs, isSubmitting } = useGetContext(SignInFormContext);
+  const { t } = useSignInTranslations();
+
+  return (
+    <div className="flex flex-col gap-3">
+      <SignInDontHaveAccount />
+      <FormBtn isLoading={isSubmitting} text={t("signInBtn")} />
+      <FormErr content={formErrs.root?.server.message} />
+    </div>
+  );
+}
