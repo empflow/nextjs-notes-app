7e1f087689c032962085961bae6b8081df40eb3b
Matvey <empflow@tutanota.com>
2023-09-01 16:42:35 +0300

    feat(client): make protectedPage() usable in client components

--- client/src/utils/protectedPage.ts
+++ client/src/utils/protectedPage.ts
@@ -1,9 +1,18 @@
+import Cookies from "js-cookie";
 import { cookies } from "next/dist/client/components/headers";
 import { redirect } from "next/navigation";
+import isSsr from "./isSsr";
+import { TCookie } from "./types";
 
 export default function protectedPage() {
-  const cookieStore = cookies();
-  const refreshToken = cookieStore.get("refreshToken");
+  let refreshToken: TCookie | undefined;
+
+  if (isSsr()) {
+    const cookieStore = cookies();
+    refreshToken = cookieStore.get("refreshToken");
+  } else {
+    refreshToken = Cookies.get("refreshToken");
+  }
 
   if (!refreshToken) {
     return redirect("/auth/sign-in");
