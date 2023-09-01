8a522105a3f49c4c9587b94a8fde9107c5b25b2e
Matvey <empflow@tutanota.com>
2023-09-01 14:35:53 +0300

    feat(client): add <PasswordInput /> for <SignInForm />

---
+++ client/src/app/[locale]/(main)/auth/sign-in/components/PasswordInput.tsx
@@ -0,0 +1,29 @@
+import Input from "@/app/components/form/Input";
+import useGetContext from "@/app/hooks/useGetContext";
+import SignInFormContext from "@/contexts/SignInFormContext";
+import { minPasswordLength, maxPasswordLength } from "@shared/values";
+import useSignInTranslations from "./hooks/useFormTranslations";
+
+export default function SignInPasswordInput() {
+  const { formT } = useSignInTranslations();
+  const { register, formErrs } = useGetContext(SignInFormContext);
+
+  return (
+    <Input
+      label={formT("password")}
+      register={register("password", {
+        required: formT("noPassword"),
+        minLength: {
+          value: minPasswordLength,
+          message: formT("passwordTooShort", { pwdLength: minPasswordLength }),
+        },
+        maxLength: {
+          value: maxPasswordLength,
+          message: formT("passwordTooLong", { pwdLength: maxPasswordLength }),
+        },
+      })}
+      type="password"
+      errMsg={formErrs.password?.message}
+    />
+  );
+}
