9a7763349ce170255caea4ee36ee227c334f54a8
Matvey <empflow@tutanota.com>
2023-09-03 16:49:12 +0300

    feat(client): add useCommonNotifications.ts

---
+++ client/src/app/hooks/useCommonNotifications.ts
@@ -0,0 +1,12 @@
+import notify from "@/utils/notify";
+import { useTranslations } from "next-intl";
+
+export default function useCommonNotifications() {
+  const errsT = useTranslations("Errors");
+
+  function notifyGenericErr() {
+    notify(errsT("generic"));
+  }
+
+  return { notifyGenericErr };
+}
