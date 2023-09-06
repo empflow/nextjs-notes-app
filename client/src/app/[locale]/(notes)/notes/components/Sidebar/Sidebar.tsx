6c256f498420ae5c0f9917cee05335ba8094a336
Matvey <empflow@tutanota.com>
2023-09-06 07:07:33 +0300

    feat(client): make sidebar resizable

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,23 @@
 import NotesList from "./NotesList/NotesList";
 import Controls from "./Controls/Controls";
+import { ResizableBox } from "react-resizable";
+import "react-resizable/css/styles.css";
+import ResizeHandle from "./ResizeHandle";
 
 export default function Sidebar() {
   return (
-    <aside className="flex min-h-[100dvh] w-[350px] flex-col gap-2 border-r border-light-3.5xl-gray p-5 dark:border-dark-4xl-gray">
-      <Controls />
-      <NotesList />
-    </aside>
+    <ResizableBox
+      width={300}
+      axis="x"
+      className="flex min-h-[100dvh]"
+      maxConstraints={[700, 0]}
+      minConstraints={[180, 0]}
+      handle={<ResizeHandle />}
+    >
+      <aside className="flex w-full flex-col gap-2 p-5">
+        <Controls />
+        <NotesList />
+      </aside>
+    </ResizableBox>
   );
 }
