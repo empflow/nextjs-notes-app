16498725318085a9ae63fa60b0dc59a10f765912
Matvey <empflow@tutanota.com>
2023-09-08 19:52:13 +0300

    refactor(client): clean up <NotesList />

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/NotesList/NotesList.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/NotesList/NotesList.tsx
@@ -1,20 +1,7 @@
-import BigBtn from "@/app/components/buttons/Big";
-import MediumBtn from "@/app/components/buttons/Medium";
-import SmallBtn from "@/app/components/buttons/Small";
-import TinyBtn from "@/app/components/buttons/Tiny";
-import useGetContext from "@/app/hooks/useGetContext";
-
-import { TWidth } from "@/utils/types";
-import { useTranslations } from "next-intl";
-import NotesContext from "@/contexts/NotesContext";
 import { ErrorBoundary } from "react-error-boundary";
 import NotesElems from "./NotesElems";
 
 export default function NotesList() {
-  const context = useGetContext(NotesContext);
-  const { notes, selectedNoteId, setSelectedNoteId, notesLoading } = context;
-  const t = useTranslations("Notes");
-
   return (
     <div className="flex flex-grow flex-col">
       <ErrorBoundary fallback={<>Something went wrong</>}>
