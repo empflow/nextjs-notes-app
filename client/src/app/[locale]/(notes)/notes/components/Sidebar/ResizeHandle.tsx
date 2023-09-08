9f10b2b77886e88f1aa321e1b9d9191a54458b8e
Matvey <empflow@tutanota.com>
2023-09-08 16:21:01 +0300

    feat(client): make sidebar resize handle draggable area larger but leave the layout unaffected

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/ResizeHandle.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/ResizeHandle.tsx
@@ -1,13 +1,19 @@
 import { forwardRef } from "react";
 
+// ref and {...restProps} should be on the element which will need to be dragged to resize whatever you're resizing
+
+// here the parent is 1px wide but the draggable element inside it is 5px wide
+// so this changes the layout by just 1 pixel but the draggable area is 5 pixels
 const ResizeHandle = forwardRef<any, any>((props, ref) => {
   const { handleAxis, ...restProps } = props;
   return (
-    <div
-      ref={ref}
-      className="border-2 border-r border-light-3.5xl-gray hover:cursor-ew-resize dark:border-dark-4xl-gray"
-      {...restProps}
-    ></div>
+    <div className="relative flex w-[1px] bg-light-3.5xl-gray dark:bg-dark-4xl-gray">
+      <div
+        className="absolute bottom-0 left-[-5px] top-0 w-[10px] hover:cursor-ew-resize"
+        ref={ref}
+        {...restProps}
+      ></div>
+    </div>
   );
 });
 
