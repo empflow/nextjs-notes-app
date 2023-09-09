30017c6afc32b984057b17c35fb0a83591f14e63
Matvey <empflow@tutanota.com>
2023-09-09 16:36:48 +0300

    feat(server): don't return bad request when requesting a note but providing an invalid id

--- server/src/middleware/notes/validateGetNote.ts
+++ server/src/middleware/notes/validateGetNote.ts
@@ -1,13 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { errMsgs } from "../../config/globalVars";
-import throwIfInvalidObjectId from "../../utils/throwers/throwIfInvalidObjectId";
 
 export default async function validateGetNote(
-  req: Request,
-  res: Response,
+  _req: Request,
+  _res: Response,
   next: NextFunction
 ) {
-  const { noteId } = req.params;
-  throwIfInvalidObjectId(noteId, { msg: errMsgs.noteInvalidId });
   next();
 }
