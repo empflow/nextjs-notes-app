5cf77cd4dbcc3553d50b65e6cf8289847d7f46dc
Matvey <empflow@tutanota.com>
2023-09-09 14:31:14 +0300

    refactor(client): rejectedRespInterceptor.ts

--- client/src/utils/http/httpWithAuth/rejectedRespInterceptor.ts
+++ client/src/utils/http/httpWithAuth/rejectedRespInterceptor.ts
@@ -1,4 +1,5 @@
 import clearAuthData from "@/utils/clearAuthData";
+import { TAxiosErrWithResp } from "@/utils/types";
 import { TErrCode } from "@shared/types";
 import axios, { AxiosRequestConfig } from "axios";
 import isAxiosErrWithResp from "../../isAxiosErrWithResp";
@@ -7,15 +8,10 @@ import convertAccessTokenToAuthHeader from "./convertAccessTokenToAuthHeader";
 import getAndStoreNewTokens from "./getAndStoreNewTokens";
 
 export default async function rejectedRespInterceptor(err: any) {
-  if (
-    !isAxiosErrWithResp(err) ||
-    err.response.data.errCode !== TErrCode.INVALID_ACCESS_TOKEN
-  ) {
-    return Promise.reject(err);
-  }
+  if (!canHandleErr(err)) return Promise.reject(err);
 
   const newTokens = await getAndStoreNewTokens();
-  if (!newTokens || !err.config) {
+  if (!newTokens) {
     clearAuthData();
     redirToSignIn();
     return Promise.reject(err);
@@ -23,13 +19,32 @@ export default async function rejectedRespInterceptor(err: any) {
 
   try {
     const authToken = convertAccessTokenToAuthHeader(newTokens.accessToken);
-    const reqConfig: AxiosRequestConfig = {
-      ...err.config,
-      headers: { ...err.config.headers, Authorization: authToken },
-    };
-    const resp = await axios(reqConfig);
-    return resp;
+    const reqConfig = getReqConfig(err, authToken);
+    return axios(reqConfig);
   } catch (err) {
     return Promise.reject(err);
   }
+}
+
+type ReturnTCanHandleErr = TAxiosErrWithResp & {
+  config: AxiosRequestConfig;
+  response: { data: { errCode: TErrCode.INVALID_ACCESS_TOKEN } };
+};
+function canHandleErr(err: any): err is ReturnTCanHandleErr {
+  if (
+    !isAxiosErrWithResp(err) ||
+    err.response.data.errCode !== TErrCode.INVALID_ACCESS_TOKEN ||
+    !err.config
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function getReqConfig(err: ReturnTCanHandleErr, authToken: string) {
+  const reqConfig: AxiosRequestConfig = {
+    ...err.config,
+    headers: { ...err.config.headers, Authorization: authToken },
+  };
+  return reqConfig;
 }
