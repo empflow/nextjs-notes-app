5a304eba8a26f19ed987b6f83a4d192c72e63b3d
Matvey <empflow@tutanota.com>
2023-09-10 17:19:37 +0300

    feat(client): increase gap between tag name and add new note button

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Controls/Controls.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Controls/Controls.tsx
@@ -7,7 +7,7 @@ export default function Controls() {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-2">
           <CurrTag />
           <AddNoteBtn />
         </div>
