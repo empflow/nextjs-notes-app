5ddf9c00db551fb84513691434e578c32b71c841
Matvey <empflow@tutanota.com>
2023-09-10 14:37:36 +0300

    feat(client): replace sign in link in header with a cool button

--- client/src/app/components/Header/ProfileMenu/ProfileMenu.tsx
+++ client/src/app/components/Header/ProfileMenu/ProfileMenu.tsx
@@ -10,7 +10,13 @@ export default function ProfileMenu() {
   const refreshToken = cookiesStore.get("refreshToken")?.value ?? null;
 
   if (!username || !refreshToken) {
-    return <Link href="/auth/sign-in">Sign in</Link>;
+    return (
+      <Link href="/auth/sign-in" className="no-underline">
+        <button className="rounded border-2 border-l-accent px-2 py-1 text-l-accent duration-100 hover:bg-l-accent hover:text-white dark:border-light-3xl-blue dark:text-light-3xl-blue dark:hover:bg-light-3xl-blue dark:hover:text-black">
+          Sign in
+        </button>
+      </Link>
+    );
   }
 
   return <ProfileMenuContent signedInAs={username || t("signedInAsUnknown")} />;
