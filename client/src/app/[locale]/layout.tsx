c5774db462b0cb7b5b521a4a70fbf71657775e18
Matvey <empflow@tutanota.com>
2023-09-10 17:32:39 +0300

    refactor(client): abstract react-loading-skeleton css styles into its own file

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import "../globals.css";
+import "../globalStyles/reactLoadingSkeleton.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loading-skeleton/dist/skeleton.css";
 import { NextIntlClientProvider, useLocale } from "next-intl";
