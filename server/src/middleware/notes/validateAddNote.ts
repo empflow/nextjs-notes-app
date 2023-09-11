d8060b091939a561594fccc640e6a18f6fa5d91a
Matvey <empflow@tutanota.com>
2023-09-11 16:52:05 +0300

    feat(server): allow adding notes without title and/or content

--- server/src/middleware/notes/validateAddNote.ts
+++ server/src/middleware/notes/validateAddNote.ts
@@ -1,14 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 export default async function validateAddNote(
-  req: Request,
-  res: Response,
+  _req: Request,
+  _res: Response,
   next: NextFunction
 ) {
-  const { title, content } = req.body;
-  if (!title || !content) res.status(400);
-  if (!title) res.json({ message: "Note title not provided" });
-  if (!content) res.json({ message: "Note content not provided" });
-
   next();
 }
