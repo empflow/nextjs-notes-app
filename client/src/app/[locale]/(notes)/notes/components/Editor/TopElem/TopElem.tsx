a2bb988d949b02aed8b0e5d6f8197c81db5c5139
Matvey <empflow@tutanota.com>
2023-09-14 17:14:12 +0300

    feat(client): add <TopElem />
    This displays either when the note was created or when it was last modified. Will be used inside the <Editor /> element.

---
+++ client/src/app/[locale]/(notes)/notes/components/Editor/TopElem/TopElem.tsx
@@ -0,0 +1,40 @@
+import { useLocale, useTranslations } from "next-intl";
+import { ReactNode, useState } from "react";
+import useGetTimeAgoFormatted from "./hooks/useGetTimeAgoFormatted";
+
+export type TTopElemState = "createdAt" | "modifiedAt";
+
+interface TProps {
+  createdAt: string;
+  updatedAt: string;
+}
+
+type TState = "createdAt" | "updatedAt";
+
+export default function TopElem({ createdAt, updatedAt }: TProps) {
+  const [state, setState] = useState<TState>("createdAt");
+  const locale = useLocale();
+  const t = useTranslations("Notes");
+  const createdAtFormatted = useGetTimeAgoFormatted(createdAt, locale);
+  const updatedAtFormatted = useGetTimeAgoFormatted(updatedAt, locale);
+  let content: ReactNode;
+
+  if (state === "createdAt") {
+    content = `${t("created")} ${createdAtFormatted}`;
+  } else {
+    content = `${t("updated")} ${updatedAtFormatted}`;
+  }
+
+  function handleClick() {
+    setState((prev) => (prev === "createdAt" ? "updatedAt" : "createdAt"));
+  }
+
+  return (
+    <div
+      className="flex cursor-pointer justify-center text-dark-gray dark:text-gray"
+      onClick={handleClick}
+    >
+      <>{content}</>
+    </div>
+  );
+}
