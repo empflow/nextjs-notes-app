4e817c05961ee16a036c6dacd2f110934ccceaa3
Matvey <empflow@tutanota.com>
2023-09-14 17:12:14 +0300

    feat(client): add getTimeAgoFormatted.ts

---
+++ client/src/utils/getTimeAgoFormatted.ts
@@ -0,0 +1,32 @@
+import isDateValid from "./isDateValid";
+
+type TTimeDivisions = { amount: number; name: Intl.RelativeTimeFormatUnit }[];
+const DIVISIONS: TTimeDivisions = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+];
+
+type GetFormattedDatesReturnT = string | null;
+export default function getTimeAgoFormatted(
+  date: string | Date,
+  locale?: string,
+): GetFormattedDatesReturnT {
+  const f = new Intl.RelativeTimeFormat(locale, { numeric: "auto" });
+  if (!isDateValid(date)) return null;
+  date = new Date(date);
+  let durationSecs = (date.getTime() - Date.now()) / 1000;
+
+  for (const division of DIVISIONS) {
+    if (Math.abs(durationSecs) < division.amount) {
+      return f.format(Math.round(durationSecs), division.name);
+    }
+    durationSecs /= division.amount;
+  }
+
+  return null;
+}
