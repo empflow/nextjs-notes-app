dfe6fd81a700dcc858d6e813b9bdb6627f25ca89
Matvey <empflow@tutanota.com>
2023-09-14 17:14:47 +0300

    feat(client): add <TopElem /> to <Editor />
    Now the user can see when they created/modified their note

--- client/src/app/[locale]/(notes)/notes/components/Editor/Editor.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Editor/Editor.tsx
@@ -1,13 +1,11 @@
 import useNoteQuery from "@/app/hooks/queries/useNoteQuery";
 import useGetContext from "@/app/hooks/useGetContext";
 import NotesContext from "@/contexts/NotesContext";
-import { useState } from "react";
+import { ReactNode } from "react";
 import EditorContent from "./EditorContent";
-
-type TTopElem = "createdAt" | "modifiedAt";
+import TopElem from "./TopElem/TopElem";
 
 export default function Editor() {
-  const [topElem, setTopElem] = useState<TTopElem>("createdAt");
   const { selectedNote } = useGetContext(NotesContext);
 
   const {
@@ -19,16 +17,22 @@ export default function Editor() {
   if (!selectedNote) return <div>Select a note to start editing</div>;
   if (isNoteErr) return <div>An error has occurred</div>;
 
+  let content: ReactNode;
+
+  if (isNoteLoading) {
+    content = <div>Loading note...</div>;
+  } else {
+    content = (
+      <>
+        <TopElem createdAt={note.createdAt} updatedAt={note.updatedAt} />
+        <EditorContent initContent={note.content} />
+      </>
+    );
+  }
+
   return (
     <div className="flex flex-grow flex-col p-global sm:p-global-sm">
-      <div className="flex justify-center text-dark-gray dark:text-gray">
-        some data
-      </div>
-      {isNoteLoading ? (
-        <div>Loading note...</div>
-      ) : (
-        <EditorContent initContent={note.content} />
-      )}
+      {content}
     </div>
   );
 }
