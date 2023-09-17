1a9ea42c2993844e2705bdbec0d34aac046113de
Matvey <empflow@tutanota.com>
2023-09-17 12:47:13 +0300

    refactor(server): remove unused validators

--- server/src/middleware/notes/validateGetNote.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export default async function validateGetNote(
-  _req: Request,
-  _res: Response,
-  next: NextFunction
-) {
-  next();
-}
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -4,20 +4,18 @@ import deleteNote from "../controllers/notes/deleteNote";
 import getNote from "../controllers/notes/getNote";
 import getNotesMeta from "../controllers/notes/getNotesMeta";
 import updateNote from "../controllers/notes/updateNote";
-import validateAddNote from "../middleware/notes/validateAddNote";
 import validateDeleteNote from "../middleware/notes/validateDeleteNote";
-import validateGetNote from "../middleware/notes/validateGetNote";
 import noteGetTitleAndDescription from "../middleware/notes/getTitleAndDescription";
 import updateNoteCheckNoteId from "../middleware/notes/updateNote/checkNoteId";
 import updateNoteCheckNewDataProvided from "../middleware/notes/updateNote/checkNewDataProvided";
 const router = express.Router();
 
 router.get("/", getNotesMeta);
-router.post("/add", validateAddNote, noteGetTitleAndDescription, addNote);
+router.post("/add", noteGetTitleAndDescription, addNote);
 
 router
   .route("/:noteId")
-  .get(validateGetNote, getNote)
+  .get(getNote)
   .patch(
     updateNoteCheckNoteId,
     updateNoteCheckNewDataProvided,
