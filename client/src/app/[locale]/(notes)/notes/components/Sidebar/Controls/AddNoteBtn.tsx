c00f29e11d49acd5598fd9bd74de95595422a9e0
Matvey <empflow@tutanota.com>
2023-09-19 15:47:08 +0300

    feat(client): show loading indicator when creating note

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Controls/AddNoteBtn.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Controls/AddNoteBtn.tsx
@@ -1,13 +1,22 @@
 import BigBtn from "@/app/components/buttons/Big";
+import Loading from "@/app/components/Loading";
 import useAddNewNoteMutation from "@/app/hooks/queries/useAddNoteMutation";
 import AddIcon from "@/icons/svg/addCircle.svg";
+import { ReactNode } from "react";
 
 export default function AddNoteBtn() {
   const { mutate: addNewNote, isLoading } = useAddNewNoteMutation();
+  let content: ReactNode;
+
+  if (isLoading)
+    content = (
+      <Loading foregroundColor="white" backgroundColor="#245e98" pxSize={24} />
+    );
+  else content = <AddIcon fill="white" />;
 
   return (
     <BigBtn style={{ padding: "8px" }} onClick={() => addNewNote()}>
-      <AddIcon fill="white" />
+      {content}
     </BigBtn>
   );
 }
