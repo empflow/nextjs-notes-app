b06abf25e517cba65b7c4e4a82f369ada3d24409
Matvey <empflow@tutanota.com>
2023-09-19 15:16:50 +0300

    perf(client): don't sort notes as often
    this still absolutely sucks because it sorts all the notes on every content update of any note

--- client/src/app/hooks/useSortNotes.ts
+++ client/src/app/hooks/useSortNotes.ts
@@ -1,15 +1,26 @@
 import { TNotesListNotesMeta } from "@/contexts/NotesContext";
+import { useMemo, useState } from "react";
+import copyVal from "@shared/utils/copyVal";
 
 export default function useSortNotes(notes: TNotesListNotesMeta) {
-  if (!notes) return null;
+  const [sortedNotes, setSortedNotes] = useState<TNotesListNotesMeta>(null);
 
-  notes.sort((a, b) => {
-    const updatedAtA = new Date(a.updatedAt);
-    const updatedAtB = new Date(b.updatedAt);
+  useMemo(() => {
+    if (!notes) return;
+    const unsortedNotesCopy = copyVal(notes);
+    console.log("sort");
 
-    if (updatedAtA > updatedAtB) return -1;
-    if (updatedAtB > updatedAtA) return 1;
-    return 0;
-  });
-  return notes;
+    const sortedNotes = unsortedNotesCopy.sort((a, b) => {
+      const updatedAtA = new Date(a.updatedAt);
+      const updatedAtB = new Date(b.updatedAt);
+
+      if (updatedAtA > updatedAtB) return -1;
+      if (updatedAtB > updatedAtA) return 1;
+      return 0;
+    });
+
+    setSortedNotes(sortedNotes);
+  }, [notes]);
+
+  return sortedNotes;
 }
