efaf88d8f27f4ba44c679987434e89229e71d36a
Matvey <empflow@tutanota.com>
2023-09-20 16:02:28 +0300

    feat(client): update getTheme.ts
    now also returns the value `theme`

--- client/src/utils/getTheme.ts
+++ client/src/utils/getTheme.ts
@@ -2,6 +2,7 @@ interface ReturnT {
   isSystem: boolean;
   isLight: boolean;
   isDark: boolean;
+  theme: "system" | "dark" | "light";
 }
 
 /**
@@ -13,13 +14,16 @@ export default function getTheme(theme: string | undefined): ReturnT {
     isLight: false,
     isDark: false,
     isSystem: false,
+    theme: "system",
   };
 
   if (theme !== "light" && theme !== "dark" && theme !== "system") {
     result.isSystem = true;
+    result.theme = "system";
     return result;
   }
 
+  result.theme = theme;
   result.isSystem = theme === "system";
   result.isLight = theme === "light";
   result.isDark = theme === "dark";
