bcd3978e51531a4592fd9838feba0e90b32f3a96
Matvey <empflow@tutanota.com>
2023-09-20 13:26:14 +0300

    feat(client): add <ProfileMenuSignIn />

---
+++ client/src/app/components/Header/ProfileMenu/SignIn.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function ProfileMenuSignIn() {
+  return (
+    <Link href="/auth/sign-in" className="no-underline">
+      <button className="rounded border-2 border-l-accent px-2 py-1 text-l-accent duration-100 hover:bg-l-accent hover:text-white dark:border-light-3xl-blue dark:text-light-3xl-blue dark:hover:bg-light-3xl-blue dark:hover:text-black">
+        Sign in
+      </button>
+    </Link>
+  );
+}
