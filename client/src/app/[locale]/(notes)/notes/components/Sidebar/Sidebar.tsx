a86daa6e21e1c56b696c900f19d95b0415112eb0
Matvey <empflow@tutanota.com>
2023-09-20 13:27:43 +0300

    feat(client): use <SidebarResizableBoxProviders /> in <Sidebar />

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -1,26 +1,18 @@
 import NotesList from "./NotesList/NotesList";
 import Controls from "./Controls/Controls";
-import { ResizableBox } from "react-resizable";
 import "react-resizable/css/styles.css";
-import ResizeHandle from "./ResizeHandle";
 import NotesListContainer from "./NotesList/NotesListContainer";
+import SidebarResizableBoxProviders from "./ResizableBoxProviders";
 
 export default function Sidebar() {
   return (
-    <ResizableBox
-      width={400}
-      axis="x"
-      className="flex h-[100dvh]"
-      maxConstraints={[500, 0]}
-      minConstraints={[180, 0]}
-      handle={<ResizeHandle />}
-    >
+    <SidebarResizableBoxProviders>
       <aside className="flex w-full flex-col gap-3 overflow-auto">
         <Controls />
         <NotesListContainer>
           <NotesList />
         </NotesListContainer>
       </aside>
-    </ResizableBox>
+    </SidebarResizableBoxProviders>
   );
 }
