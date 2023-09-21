978a46b6452dd17c153236d7647e3312f8a265f4
Matvey <empflow@tutanota.com>
2023-09-21 19:07:06 +0300

    feat(client): add Popover.tsx

---
+++ client/src/app/components/Popover.tsx
@@ -0,0 +1,42 @@
+import { SetState } from "@/utils/types";
+import { ReactNode, useEffect, useRef } from "react";
+
+interface TProps {
+  children: ReactNode;
+  state: [boolean, SetState<boolean>];
+}
+
+export default function Popover({
+  children,
+  state: [isOpen, setIsOpen],
+}: TProps) {
+  const popoverRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    document.addEventListener("click", onClickOutsideDropdown);
+    return () => document.removeEventListener("click", onClickOutsideDropdown);
+  }, []);
+
+  function onClickOutsideDropdown(e: globalThis.MouseEvent) {
+    const popover = popoverRef.current;
+    if (!(e.target instanceof Node)) return;
+
+    const isClickOutsidePopover = !popover?.contains(e.target);
+    if (isClickOutsidePopover) setIsOpen(false);
+  }
+
+  return (
+    <div
+      className={`relative border border-red-500 duration-100 ${
+        isOpen ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <div
+        className="absolute left-1/2 -translate-x-1/2 rounded border border-white"
+        ref={popoverRef}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
