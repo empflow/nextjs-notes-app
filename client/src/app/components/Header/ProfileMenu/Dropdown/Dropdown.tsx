d5910d99bfcfa21daedf6bada480744bd9519b13
Matvey <empflow@tutanota.com>
2023-09-22 17:16:09 +0300

    feat(client): add close btn to prof. menu dropdown

--- client/src/app/components/Header/ProfileMenu/Dropdown/Dropdown.tsx
+++ client/src/app/components/Header/ProfileMenu/Dropdown/Dropdown.tsx
@@ -2,6 +2,7 @@ import { forwardRef } from "react";
 import useGetContext from "@/app/hooks/useGetContext";
 import ProfileMenuDropdownBtnsList from "./DropdownBtnsList";
 import ProfileMenuContext from "@/contexts/ProfileMenu";
+import ProfileMenuDropdownCloseBtn from "./CloseBtn";
 
 const ProfileMenuDropdown = forwardRef<HTMLDivElement>((_props, ref) => {
   const profileMenuContext = useGetContext(ProfileMenuContext);
@@ -10,15 +11,20 @@ const ProfileMenuDropdown = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
     <div
       ref={ref}
-      className={`absolute left-0 right-0 top-[70px] z-10 bg-transparent px-global duration-200 dark:shadow-none sm:left-auto sm:top-[56px] sm:w-[300px] sm:px-0 ${
+      className={`absolute left-0 right-0 top-0 z-10 bg-transparent duration-200 dark:shadow-none sm:left-auto sm:w-[300px] sm:px-global ${
         isDropdownOpen
           ? "pointer-events-auto translate-y-0 opacity-100"
           : "pointer-events-none -translate-y-1 opacity-0"
       }`}
     >
       <div className="flex w-full flex-col gap-3 rounded border-light-2xl-gray bg-l-secondary px-3 py-5 shadow-md dark:border-dark-3xl-gray dark:bg-d-secondary">
-        <div className="px-2 font-medium">{signedInAs}</div>
-        <ProfileMenuDropdownBtnsList />
+        <div>
+          <div className="flex justify-between px-2 font-medium">
+            <p className="truncate">{signedInAs}</p>
+            <ProfileMenuDropdownCloseBtn />
+          </div>
+          <ProfileMenuDropdownBtnsList />
+        </div>
       </div>
     </div>
   );
