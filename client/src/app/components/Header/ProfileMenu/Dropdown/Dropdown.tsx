7b6bae84b8c0c4754a1c905977bd767097fe5dbd
Matvey <empflow@tutanota.com>
2023-09-22 16:56:05 +0300

    fix(client): add z-index to profile menu dropdown
    to prevent the cursor from passing through it

--- client/src/app/components/Header/ProfileMenu/Dropdown/Dropdown.tsx
+++ client/src/app/components/Header/ProfileMenu/Dropdown/Dropdown.tsx
@@ -10,7 +10,7 @@ const ProfileMenuDropdown = forwardRef<HTMLDivElement>((_props, ref) => {
   return (
     <div
       ref={ref}
-      className={`absolute left-0 right-0 top-[70px] bg-transparent px-global duration-200 dark:shadow-none sm:left-auto sm:top-[56px] sm:w-[300px] sm:px-0 ${
+      className={`absolute left-0 right-0 top-[70px] z-10 bg-transparent px-global duration-200 dark:shadow-none sm:left-auto sm:top-[56px] sm:w-[300px] sm:px-0 ${
         isDropdownOpen
           ? "pointer-events-auto translate-y-0 opacity-100"
           : "pointer-events-none -translate-y-1 opacity-0"
