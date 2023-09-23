5241ef8d30d0091cc0ab6e4f5226ba8396b43a3a
Matvey <empflow@tutanota.com>
2023-09-23 18:20:03 +0300

    fix(client): Tooltip
    1. fixed all tooltips inside a TooltipContainer showing up all at once
    2. fixed dark mode styling

--- client/src/app/components/Tooltip.tsx
+++ client/src/app/components/Tooltip.tsx
@@ -1,5 +1,5 @@
 import TooltipContainerContext from "@/contexts/TooltipContainerContext";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import useGetContext from "../hooks/useGetContext";
 
 interface TProps {
@@ -8,15 +8,21 @@ interface TProps {
 }
 
 export default function Tooltip({ children, text }: TProps) {
+  const [isCursorHovering, setIsCursorHovering] = useState(false);
   const { showTooltips } = useGetContext(TooltipContainerContext);
+  const show = isCursorHovering && showTooltips;
 
   return (
-    <div className="relative">
+    <div
+      className="relative"
+      onMouseEnter={() => setIsCursorHovering(true)}
+      onMouseLeave={() => setIsCursorHovering(false)}
+    >
       <div>{children}</div>
 
       <div
-        className={`absolute left-1/2 w-max max-w-[250px] -translate-x-1/2 rounded border border-light-3xl-gray bg-white px-[5px] py-[2px] text-center shadow-lg duration-200 ${
-          showTooltips ? "opacity-100" : "opacity-0"
+        className={`absolute left-1/2 w-max max-w-[250px] -translate-x-1/2 rounded border border-light-3xl-gray bg-l-secondary px-[5px] py-[2px] text-center shadow-lg duration-200 dark:border-dark-3xl-gray dark:bg-d-main dark:text-slate-300 ${
+          show ? "opacity-100" : "opacity-0"
         }`}
       >
         <p>{text}</p>
