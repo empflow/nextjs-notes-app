6f1f40bea8154e707eefb5bd9ebb98e088c8de17
Matvey <empflow@tutanota.com>
2023-09-24 16:23:23 +0300

    feat(client): add <TextFormat /> toolbar item

---
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/TextFormat/TextFormat.tsx
@@ -0,0 +1,33 @@
+import ViewsContainer from "@/app/components/Views/ViewsContainer";
+import useGetContext from "@/app/hooks/useGetContext";
+import { ToolbarContext } from "../../Context";
+import FormatTextIcon from "@/icons/svg/formatText.svg";
+import ToolbarItem from "../Item";
+import ToolbarTextFormatMainView from "./Main";
+import ToolbarTextFormatSettingsView from "./Settings";
+import { useTranslations } from "next-intl";
+
+export default function TextFormat() {
+  const t = useTranslations("Toolbar.tooltips");
+  const { isFormatTextMenuOpen: isOpen, setIsFormatTextMenuOpen: setIsOpen } =
+    useGetContext(ToolbarContext);
+
+  return (
+    <ToolbarItem
+      icon={<FormatTextIcon />}
+      onClick={() => setIsOpen((prev) => !prev)}
+      tooltipText={t("formatText")}
+      hideTooltip={isOpen}
+    >
+      <ViewsContainer
+        centered={true}
+        top={40}
+        {...{ isOpen, setIsOpen }}
+        initMenu="main"
+      >
+        <ToolbarTextFormatMainView />
+        <ToolbarTextFormatSettingsView />
+      </ViewsContainer>
+    </ToolbarItem>
+  );
+}
