bbf697060e64937930d0b66ae9906e950a838256
Matvey <empflow@tutanota.com>
2023-09-24 16:16:40 +0300

    feat(client): add ViewsContainer.tsx

---
+++ client/src/app/components/Views/ViewsContainer.tsx
@@ -0,0 +1,55 @@
+import { createContext, ReactNode, useState } from "react";
+import { SetState } from "@/utils/types";
+import { TViewsContainerContext } from "@/contexts/ViewsContainerContext";
+
+export type TViewsContainerHeight = "auto" | number;
+
+export const ViewsContainerContext =
+  createContext<TViewsContainerContext>(null);
+
+interface TProps {
+  initMenu: string;
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: SetState<boolean>;
+  centered?: boolean;
+  top?: number;
+}
+
+export const transitionDuration = 150;
+
+export default function ViewsContainer({
+  initMenu,
+  children,
+  isOpen,
+  setIsOpen,
+  centered = true,
+  top,
+}: TProps) {
+  const [activeView, setActiveView] = useState(initMenu);
+  const [height, setHeight] = useState<TViewsContainerHeight>("auto");
+
+  return (
+    <div
+      className={`${
+        isOpen
+          ? "pointer-events-auto translate-y-0 opacity-100"
+          : "pointer-events-none -translate-y-1 opacity-0"
+      } ${
+        centered ? "left-1/2 -translate-x-1/2" : ""
+      } absolute top-0 z-10 box-content w-[300px] rounded border border-light-3xl-gray bg-l-secondary p-2 shadow-md duration-200 dark:border-dark-3xl-gray dark:bg-d-main`}
+      style={{ height, top }}
+    >
+      <ViewsContainerContext.Provider
+        value={{
+          activeView,
+          setActiveView,
+          setViewsContainerHeight: setHeight,
+          setIsViewsContainerOpen: setIsOpen,
+        }}
+      >
+        {children}
+      </ViewsContainerContext.Provider>
+    </div>
+  );
+}
