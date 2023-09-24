0d28121da3ecaabaffeadbc923e0c677b337b2e6
Matvey <empflow@tutanota.com>
2023-09-24 16:15:09 +0300

    feat(client): improve Tooltip.tsx
    1. added optional `hide` prop
    2. disable pointer events when hidden
    3. added optional `top` props (css `top` prop)

--- client/src/app/components/Tooltip.tsx
+++ client/src/app/components/Tooltip.tsx
@@ -5,12 +5,14 @@ import useGetContext from "../hooks/useGetContext";
 interface TProps {
   children: ReactNode;
   text: string;
+  hide?: boolean;
+  top?: number;
 }
 
-export default function Tooltip({ children, text }: TProps) {
+export default function Tooltip({ children, text, hide = false, top }: TProps) {
   const [isCursorHovering, setIsCursorHovering] = useState(false);
   const { showTooltips } = useGetContext(TooltipContainerContext);
-  const show = isCursorHovering && showTooltips;
+  const show = !hide && isCursorHovering && showTooltips;
 
   return (
     <div
@@ -18,15 +20,17 @@ export default function Tooltip({ children, text }: TProps) {
       onMouseEnter={() => setIsCursorHovering(true)}
       onMouseLeave={() => setIsCursorHovering(false)}
     >
-      <div>{children}</div>
-
       <div
-        className={`absolute left-1/2 w-max max-w-[250px] -translate-x-1/2 rounded border border-light-3xl-gray bg-l-secondary px-[5px] py-[2px] text-center shadow-lg duration-200 dark:border-dark-3xl-gray dark:bg-d-main dark:text-slate-300 ${
-          show ? "opacity-100" : "opacity-0"
+        className={`absolute left-1/2 w-max max-w-[250px] -translate-x-1/2 rounded border border-light-3xl-gray bg-l-secondary px-[5px] py-[2px] text-center shadow-lg duration-200 dark:border-dark-3xl-gray dark:bg-d-main ${
+          show
+            ? "pointer-events-auto opacity-100"
+            : "pointer-events-none opacity-0"
         }`}
+        style={{ top }}
       >
         <p>{text}</p>
       </div>
+      {children}
     </div>
   );
 }
