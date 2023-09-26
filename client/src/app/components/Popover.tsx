b01dbcf6eb67f8921b40345106f1f4936c1f8da9
Matvey <empflow@tutanota.com>
2023-09-26 19:04:06 +0300

    feat&refactor(client): Popover.tsx: add more positions
    added positions like top-center or bottom-left and also now this uses an object instead of an ugly and bulky switch statement

--- client/src/app/components/Popover.tsx
+++ client/src/app/components/Popover.tsx
@@ -7,52 +7,55 @@ type TProps = {
   style?: CSSProperties;
   isOpen: boolean;
   setIsOpen: SetState<boolean>;
-  position: "top" | "bottom" | "left" | "right";
+  position:
+    | "top-center"
+    | "top-left"
+    | "top-right"
+    | "bottom-center"
+    | "bottom-left"
+    | "bottom-right"
+    | "left"
+    | "right";
   offset?: number;
+  className?: string;
 };
+
+interface TPosition {
+  translate?: string;
+  top?: string | number;
+  bottom?: string | number;
+  left?: string | number;
+  right?: string | number;
+}
 
 export default function Popover({
   children,
   style,
+  className,
   position,
   isOpen,
   setIsOpen,
   offset = 0,
 }: TProps) {
-  let translate: string | undefined,
-    top: string | number | undefined,
-    bottom: string | number | undefined,
-    left: string | number | undefined,
-    right: string | number | undefined;
-
-  switch (position) {
-    case "top":
-      translate = "-50%";
-      left = "50%";
-      bottom = offset;
-      break;
-    case "bottom":
-      translate = "-50%";
-      left = "50%";
-      top = offset;
-      break;
-    case "left":
-      translate = "0 50%";
-      bottom = "50%";
-      right = offset ?? 0;
-      break;
-    case "right":
-      translate = "0 50%";
-      bottom = "50%";
-      left = offset ?? 0;
-      break;
-  }
+  const positions: Record<typeof position, TPosition> = {
+    "top-center": { translate: "-50%", left: "50%", bottom: offset },
+    "top-left": { bottom: offset, right: 0 },
+    "top-right": { bottom: offset, left: 0 },
+    "bottom-center": { translate: "-50%", left: "50%", top: offset },
+    "bottom-left": { top: offset, right: 0 },
+    "bottom-right": { top: offset, left: 0 },
+    left: { translate: "0 50%", bottom: "50%", right: offset ?? 0 },
+    right: { translate: "0 50%", bottom: "50%", left: offset ?? 0 },
+  };
+  const activePosition = positions[position];
 
   return (
     <>
       <div
-        style={{ ...{ top, bottom, left, right, translate }, ...style }}
-        className={`absolute z-20 ${!isOpen ? "pointer-events-none" : ""}`}
+        style={{ ...activePosition, ...style }}
+        className={`absolute z-20 ${!isOpen ? "pointer-events-none" : ""} ${
+          className ?? ""
+        }`}
       >
         {children}
       </div>
