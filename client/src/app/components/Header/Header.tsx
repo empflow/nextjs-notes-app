4b0e289c21a771129ca4726b9b49d81f9c2f3509
Matvey <empflow@tutanota.com>
2023-09-27 16:56:49 +0300

    fix(client): put header over overlay & set header profile menu dropdown `top` value

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -4,13 +4,13 @@ import ProfileMenu from "./ProfileMenu/ProfileMenu";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 border-b border-light-xl-gray bg-l-header px-global py-3 backdrop-blur-lg dark:border-dark-3xl-gray dark:bg-d-header sm:px-global-sm">
+    <header className="sticky top-0 z-20 border-b border-light-xl-gray bg-l-header px-global py-3 backdrop-blur-lg dark:border-dark-3xl-gray dark:bg-d-header sm:px-global-sm">
       <div className="md m-auto flex w-full max-w-global items-center justify-between">
         <Link href="/" className="no-underline">
           <Logo />
         </Link>
 
-        <ProfileMenu />
+        <ProfileMenu dropdownTopPx={40} />
       </div>
     </header>
   );
