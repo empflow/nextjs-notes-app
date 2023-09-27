e4afd39a3642f0df4140c6653aec45dc76452332
Matvey <empflow@tutanota.com>
2023-09-27 19:08:14 +0300

    refactor(client): remove unused code from

--- client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/FormatText/Formatting/Formatting.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/FormatText/Formatting/Formatting.tsx
@@ -1,11 +1,8 @@
 import View from "@/app/components/Views/View";
-import { ViewsContainerContext } from "@/app/components/Views/ViewsContainer";
-import useGetContext from "@/app/hooks/useGetContext";
 import { useTranslations } from "next-intl";
 import TopSection from "./TopSection/TopSection";
 
 export default function FormattingView() {
-  const { setActiveView } = useGetContext(ViewsContainerContext);
   const t = useTranslations("Toolbar.formatText");
 
   return (
