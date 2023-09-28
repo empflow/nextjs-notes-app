ca271c5b50510a1fa8692a0351a7871609e4a381
Matvey <empflow@tutanota.com>
2023-09-28 18:32:55 +0300

    refactor(client): <ToolbarItem />
    use <ColoredIcon />

--- client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/Item.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/Item.tsx
@@ -1,11 +1,9 @@
+import ColoredIcon from "@/app/components/ColoredIcon";
 import Tooltip from "@/app/components/Tooltip";
-import useGetContext from "@/app/hooks/useGetContext";
-import TooltipContainerContext from "@/contexts/TooltipContainerContext";
 import { useTranslations } from "next-intl";
-import { cloneElement, MouseEvent, ReactNode } from "react";
+import { MouseEvent, ReactNode } from "react";
 
 interface TProps {
-  hideTooltip?: boolean;
   tooltipText: string;
   icon: any;
   onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
@@ -34,19 +32,11 @@ export default function ToolbarItem({
         onClick={onClick}
         disabled={isDisabled}
         className={`rounded px-[10px] py-1 disabled:cursor-not-allowed ${
-          isActive
-            ? "bg-light-4xl-gray dark:bg-dark-4xl-gray"
-            : "hover:bg-light-5xl-gray dark:hover:bg-dark-5xl-gray"
+          isActive ? "bg-light-4xl-gray dark:bg-dark-4xl-gray" : ""
         } ${className ?? ""}`}
         title={isDisabled ? `${tooltipText} (${t("disabled")})` : undefined}
       >
-        {cloneElement(icon, {
-          className: `${
-            isDisabled
-              ? "fill-light-gray dark:fill-dark-xl-gray"
-              : "fill-light-xl-blue"
-          }`,
-        })}
+        <ColoredIcon {...{ icon, isDisabled }} />
       </button>
       {children}
     </Tooltip>
