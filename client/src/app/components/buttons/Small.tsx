883fafaa1a3b82c200554ce27d08b4d1b49a0d3e
Matvey <empflow@tutanota.com>
2023-10-03 20:06:23 +0300

    feat(client): add disabled styling to <SmallBtn />

--- client/src/app/components/buttons/Small.tsx
+++ client/src/app/components/buttons/Small.tsx
@@ -9,7 +9,7 @@ const SmallBtn: React.FC<ButtonProps> = ({
   return (
     <button
       {...restProps}
-      className={`${baseClassName} px-4 py-[0.3rem] text-[0.9rem] ${className}`}
+      className={`${baseClassName} px-4 py-[0.3rem] text-[0.9rem] disabled:cursor-not-allowed disabled:bg-light-xl-gray disabled:text-dark-gray disabled:dark:bg-dark-3xl-gray disabled:dark:text-light-gray ${className}`}
     >
       {children}
     </button>
