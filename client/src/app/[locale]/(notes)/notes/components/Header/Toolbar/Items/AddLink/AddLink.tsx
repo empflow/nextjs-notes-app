420d9ed2d05f12c2506517eaec77af9786eb4dae
Matvey <empflow@tutanota.com>
2023-10-03 20:08:02 +0300

    feat(client): add AddLink.ts

---
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddLink/AddLink.tsx
@@ -0,0 +1,53 @@
+import useGetContext from "@/app/hooks/useGetContext";
+import NotesContext from "@/contexts/NotesContext";
+import { useTranslations } from "next-intl";
+import { ToolbarContext } from "../../Context";
+import ToolbarItem from "../Item";
+import LinkIcon from "@/icons/svg/link.svg";
+import Popover from "@/app/components/Popover";
+import Hidable from "@/app/components/Hidable";
+import SignleView from "@/app/components/Views/SingleView";
+import AddLinkPopoverContent from "./PopoverContent";
+import useGetToggleLinkAttrs from "./useGetToggleLinkAttrs";
+
+export default function AddLink() {
+  const menuWidth = 250;
+  const t = useTranslations("Toolbar");
+  const { editor } = useGetContext(NotesContext);
+  const { isAddLinkMenuOpen: isOpen, setIsAddLinkMenuOpen: setIsOpen } =
+    useGetContext(ToolbarContext);
+  const toggleLinkAttrs = useGetToggleLinkAttrs();
+  const isDisabled = !editor
+    ?.can()
+    .chain()
+    .focus()
+    .toggleLink(toggleLinkAttrs)
+    .run();
+
+  function toggleAddLinkMenu() {
+    setIsOpen((prev) => !prev);
+  }
+
+  return (
+    <ToolbarItem
+      tooltipText={t("tooltips.addLink")}
+      isDisabled={isDisabled}
+      hideTooltip={isOpen}
+      onClick={toggleAddLinkMenu}
+      icon={<LinkIcon />}
+    >
+      <Popover
+        style={{ width: menuWidth }}
+        position="bottom-center"
+        {...{ isOpen, setIsOpen }}
+        offset={40}
+      >
+        <Hidable className="px-3 py-2" width={menuWidth} isOpen={isOpen}>
+          <SignleView name={t("link.menuTitle")}>
+            <AddLinkPopoverContent />
+          </SignleView>
+        </Hidable>
+      </Popover>
+    </ToolbarItem>
+  );
+}
