323456670dd8171d3cb36ef71e456cd3b4509795
Matvey <empflow@tutanota.com>
2023-10-10 18:16:57 +0300

    fix(client): update config to allow images from s3

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,14 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.tebi.io",
+      },
+    ],
+  },
   experimental: {
     externalDir: true,
   },
