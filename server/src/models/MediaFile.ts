a8ad45691c176290d2334f3875ceeb663cabee5f
Matvey <empflow@tutanota.com>
2023-10-13 18:14:56 +0300

    feat(server): add MediaFile schema

---
+++ server/src/models/MediaFile.ts
@@ -0,0 +1,29 @@
+import { TMediaFileSchemaBase } from "@/shared/schemas/mediaFile";
+import mongoose, { Schema, Types } from "mongoose";
+
+export interface TMediaFileServer extends TMediaFileSchemaBase {
+  owner: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const MediaFileSchema = new Schema<TMediaFileServer>(
+  {
+    blurUrl: {
+      type: String,
+      default: null,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const MediaFile = mongoose.model<TMediaFileServer>(
+  "MediaFile",
+  MediaFileSchema
+);
+
+export default MediaFile;
