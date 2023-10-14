8403db8635ea6affb61effa9fd93bec4f1d7d5f2
Matvey <empflow@tutanota.com>
2023-10-14 17:51:39 +0300

    feat(client): automatically add `http://` to start of links

--- client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddLink/useGetToggleLinkAttrs.ts
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddLink/useGetToggleLinkAttrs.ts
@@ -3,5 +3,13 @@ import { ToolbarContext } from "../../Context";
 
 export default function useGetToggleLinkAttrs() {
   const { link } = useGetContext(ToolbarContext);
-  return { href: link, target: "_blank" };
+  let resolvedLink = link;
+
+  if (!link.startsWith("http://") || !link.startsWith("https://")) {
+    // the browser should automatically change the url to start with
+    // `https://` if that's the only protocol the webiste supports
+    resolvedLink = `http://${link}`;
+  }
+
+  return { href: resolvedLink, target: "_blank" };
 }
