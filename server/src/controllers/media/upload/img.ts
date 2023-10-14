b6aaef3af1b7c61c02ff945ad3479e0fca89e612
Matvey <empflow@tutanota.com>
2023-10-14 13:12:31 +0300

    feat(server): add uploadImgCreateMediaFile controller

---
+++ server/src/controllers/media/upload/img.ts
@@ -0,0 +1,21 @@
+import MediaFile from "@/server/src/models/MediaFile";
+import { Request, Response } from "express";
+
+export default async function uploadImgCreateMediaFile(
+  _req: Request,
+  res: Response
+) {
+  const {
+    img: { placeholderBase64: placeholderImgBase64 },
+    compressedImgUpload,
+    jwtPayload: { userId },
+  } = res.locals;
+
+  const mediaFile = await MediaFile.create({
+    owner: userId,
+    url: compressedImgUpload.Location,
+    placeholderImgBase64,
+  });
+
+  res.json(mediaFile);
+}
