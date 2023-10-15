b5f79b5169d5a4b98a35bb6203d1886089a4478d
Matvey <empflow@tutanota.com>
2023-10-15 12:41:16 +0300

    feat(client): add Undo.tsx
    undo toolbar item

---
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/Undo.tsx
@@ -0,0 +1,19 @@
+import useGetContext from "@/app/hooks/useGetContext";
+import NotesContext from "@/contexts/NotesContext";
+import { useTranslations } from "next-intl";
+import ToolbarItem from "./Item";
+import UndoIcon from "@/icons/svg/undo.svg";
+
+export default function Undo() {
+  const t = useTranslations("Toolbar.tooltips");
+  const { editor } = useGetContext(NotesContext);
+
+  return (
+    <ToolbarItem
+      tooltipText={t("undo")}
+      isDisabled={!editor?.can().chain().focus().undo().run()}
+      onClick={() => editor?.chain().focus().undo().run()}
+      icon={<UndoIcon />}
+    />
+  );
+}
