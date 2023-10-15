efd950b001e97f05c3365eef2324ab0cd00ce007
Matvey <empflow@tutanota.com>
2023-10-15 12:11:38 +0300

    fix(client): automatically adding protocol to links

--- client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddLink/useGetToggleLinkAttrs.ts
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddLink/useGetToggleLinkAttrs.ts
@@ -1,13 +1,13 @@
 import useGetContext from "@/app/hooks/useGetContext";
+import { linkWithProtocolRegex } from "@shared/regexes";
 import { ToolbarContext } from "../../Context";
 
 export default function useGetToggleLinkAttrs() {
   const { link } = useGetContext(ToolbarContext);
   let resolvedLink = link;
 
-  if (!link.startsWith("http://") || !link.startsWith("https://")) {
-    // the browser should automatically change the url to start with
-    // `https://` if that's the only protocol the webiste supports
+  const linkHasProtocol = linkWithProtocolRegex.test(link);
+  if (!linkHasProtocol) {
     resolvedLink = `http://${link}`;
   }
 
