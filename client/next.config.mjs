cdc7d380a952ce0050b90048549d76966df40d11
Matvey <empflow@tutanota.com>
2023-10-16 19:24:14 +0300

    refactor(client): remove commens from next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -18,28 +18,23 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
+    // svgs as react components
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg"),
     );
-
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+        resourceQuery: /url/,
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        resourceQuery: { not: /url/ },
         use: ["@svgr/webpack"],
       },
     );
-
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
