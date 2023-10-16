f740f10463f6aee61612f51a5e94e29dac1b9414
Matvey <empflow@tutanota.com>
2023-10-16 19:30:17 +0300

    refactor&feat(client): Overlay.tsx spread div attrs

--- client/src/app/components/Overlay.tsx
+++ client/src/app/components/Overlay.tsx
@@ -1,34 +1,36 @@
 "use client";
 import { SetState } from "@/utils/types";
-import { useEffect, useRef } from "react";
+import { HTMLAttributes, ReactNode, useEffect, useRef } from "react";
 import ReactDom from "react-dom";
+import useQuerySelector from "../hooks/useQuerySelector";
 
-interface TProps {
+interface TProps extends HTMLAttributes<HTMLDivElement> {
   isActive: boolean;
-  setIsActive: SetState<boolean>;
+  setIsActive?: SetState<boolean>;
   transparent?: boolean;
+  children?: ReactNode;
 }
 
 export default function Overlay({
   isActive,
   setIsActive,
   transparent = false,
+  children,
+  ...attributes
 }: TProps) {
-  const portalContainer = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    portalContainer.current = document.getElementById("popover-overlays");
-  }, []);
-
-  if (!portalContainer.current) return null;
+  const portalContainer = useQuerySelector("#popover-overlays");
+  if (!portalContainer) return null;
 
   return ReactDom.createPortal(
     <div
-      className={`absolute bottom-0 left-0 right-0 top-0 z-10 ${
+      className={`fixed bottom-0 left-0 right-0 top-0 z-10 ${
         transparent ? "bg-transparent" : "bg-black/70"
       } ${isActive ? "opacity-100" : "pointer-events-none opacity-0"}`}
-      onClick={() => setIsActive(false)}
-    ></div>,
-    portalContainer.current,
+      onClick={setIsActive ? () => setIsActive(false) : undefined}
+      {...attributes}
+    >
+      {children}
+    </div>,
+    portalContainer,
   );
 }
