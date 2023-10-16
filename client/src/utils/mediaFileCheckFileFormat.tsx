ea053215ac9fa809b23463b6aa61387ceb33690c
Matvey <empflow@tutanota.com>
2023-10-16 19:30:30 +0300

    feat(client): add mediaFileCheckFileFormat.tsx

---
+++ client/src/utils/mediaFileCheckFileFormat.tsx
@@ -0,0 +1,17 @@
+import { ALLOWED_IMG_EXTS } from "@shared/values";
+import getFileType from "./getFileType";
+import notify from "./notify";
+
+export default async function checkFileFormat(files: FileList, errMsg: string) {
+  const file = files[0];
+  if (!file) return true;
+
+  const fileType = await getFileType(file);
+  if (!ALLOWED_IMG_EXTS.includes(fileType)) {
+    console.log(fileType);
+    notify(errMsg, "error");
+    return false;
+  }
+
+  return true;
+}
