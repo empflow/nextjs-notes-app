912479a5b38389e338d04d33e5dee017e04e55a8
Matvey <empflow@tutanota.com>
2023-10-17 18:47:29 +0300

    feat(client): add useUploadMediaFileMutation.ts

---
+++ client/src/app/hooks/queries/useUploadMediaFileMutation.ts
@@ -0,0 +1,50 @@
+import httpWithAuth from "@/utils/http/httpWithAuth/httpWithAuth";
+import { useMutation } from "@tanstack/react-query";
+import useGetContext from "../useGetContext";
+import { mediaFileSchema } from "@shared/schemas/mediaFile";
+import notify from "@/utils/notify";
+import { useTranslations } from "next-intl";
+import { AddMediaFileContext } from "@/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/AddMediaFile/Context";
+import NotesContext from "@/contexts/NotesContext";
+import wait from "@/utils/wait";
+
+export default function useUploadMediaFileMutation() {
+  const errsT = useTranslations("Errors");
+  const { mediaFiles, setMenuState, setIsMenuOpen, setMediaFiles } =
+    useGetContext(AddMediaFileContext);
+  const { editor } = useGetContext(NotesContext);
+  const mutation = useMutation(mutationFn, {
+    onError: () => {
+      notify(errsT("generic"), "error");
+      setMenuState("uploadFile");
+    },
+    onMutate: () => setMenuState("uploadingFile"),
+    onSuccess: async (data) => {
+      await onSuccessMenuTeardown();
+      if (data) editor?.commands.setNextImage({ id: data._id });
+    },
+  });
+
+  async function onSuccessMenuTeardown() {
+    setIsMenuOpen(false);
+    const waitMs = 300;
+    setTimeout(() => {
+      setMenuState("chooseFile");
+      setMediaFiles(null);
+    }, waitMs);
+    await wait(waitMs);
+  }
+
+  async function mutationFn() {
+    const mediaFile = mediaFiles?.[0];
+    if (!mediaFile) return;
+
+    const formData = new FormData();
+    formData.append("img", mediaFile);
+
+    const resp = await httpWithAuth.post("/media/upload/img", formData);
+    return mediaFileSchema.parse(resp.data);
+  }
+
+  return mutation;
+}
