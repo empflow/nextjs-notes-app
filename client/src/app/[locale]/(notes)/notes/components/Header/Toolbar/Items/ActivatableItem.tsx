4be996ff60ba305db86c4fa97e9f715346d16395
Matvey <empflow@tutanota.com>
2023-10-19 14:15:01 +0300

    feat(client): add ActivatableItem.tsx

---
+++ client/src/app/[locale]/(notes)/notes/components/Header/Toolbar/Items/ActivatableItem.tsx
@@ -0,0 +1,33 @@
+import ColoredIcon from "@/app/components/ColoredIcon";
+import { ReactNode } from "react";
+
+interface TProps {
+  text: ReactNode;
+  icon?: any;
+  onClick?: () => unknown;
+  isDisabled?: boolean;
+  isActive?: boolean;
+  className?: string;
+}
+
+export default function ActivatableItem({
+  icon,
+  text,
+  className,
+  isActive,
+  isDisabled,
+  onClick,
+}: TProps) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={isDisabled}
+      className={`flex items-center gap-1 rounded p-1 text-left disabled:cursor-not-allowed ${
+        isActive ? "bg-light-4xl-gray dark:bg-dark-4xl-gray" : ""
+      } ${className ?? ""}`}
+    >
+      {icon && <ColoredIcon {...{ icon, isDisabled }} />}
+      {text}
+    </button>
+  );
+}
