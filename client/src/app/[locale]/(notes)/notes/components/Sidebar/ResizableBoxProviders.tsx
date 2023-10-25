66b06d5b6b32a965be24836ba64e6afb85fffc1c
Matvey <empflow@tutanota.com>
2023-10-25 18:47:05 +0300

    feat(cliient): optimize sub-optimal code
    made the sidebar non-resizable because I was buggy and laggy and just horrible overall!

--- client/src/app/[locale]/(notes)/notes/components/Sidebar/ResizableBoxProviders.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/ResizableBoxProviders.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import useIsScreenWidthOverBreakpoint from "@/app/hooks/useIsScreenWidthOverBreakpoint";
+import { SetState } from "@/utils/types";
 import { ReactNode } from "react";
 import { ResizableBox } from "react-resizable";
 import ResizeHandle from "./ResizeHandle";
 
 interface TProps {
   children: ReactNode;
+  width?: number;
+  setSidebarWidth: SetState<number | undefined>;
 }
 
-export default function SidebarResizableBoxProviders({ children }: TProps) {
+export default function SidebarResizableBoxProviders({
+  children,
+  width = 400,
+  setSidebarWidth,
+}: TProps) {
   const fullWidth = !useIsScreenWidthOverBreakpoint("md");
 
   if (fullWidth) return <>{children}</>;
   return (
     <ResizableBox
-      width={400}
+      onResize={(_, { size }) => setSidebarWidth(size.width)}
+      width={width}
       axis="x"
       className="flex h-[100dvh]"
       maxConstraints={[500, 0]}
--- client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
+++ client/src/app/[locale]/(notes)/notes/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,16 @@
+"use client";
 import NotesList from "./NotesList/NotesList";
 import Controls from "./Controls/Controls";
 import "react-resizable/css/styles.css";
 import NotesListContainer from "./NotesList/NotesListContainer";
-import SidebarResizableBoxProviders from "./ResizableBoxProviders";
 
 export default function Sidebar() {
   return (
-    <SidebarResizableBoxProviders>
-      <aside className="flex w-full flex-col gap-3 overflow-auto bg-light-5xl-gray dark:bg-d-secondary">
-        <Controls />
-        <NotesListContainer>
-          <NotesList />
-        </NotesListContainer>
-      </aside>
-    </SidebarResizableBoxProviders>
+    <aside className="flex w-full flex-col gap-3 overflow-auto border-light-3.5xl-gray bg-light-5xl-gray dark:border-dark-4xl-gray dark:bg-d-secondary md:max-w-[400px] md:border-r">
+      <Controls />
+      <NotesListContainer>
+        <NotesList />
+      </NotesListContainer>
+    </aside>
   );
 }
