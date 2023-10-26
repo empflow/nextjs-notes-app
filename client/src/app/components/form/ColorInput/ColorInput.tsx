9a652d9edaae4ec9b5ce3db0184e422e76deb5f2
Matvey <empflow@tutanota.com>
2023-10-26 19:44:51 +0300

    feat(client): add ColorInput.tsx

---
+++ client/src/app/components/form/ColorInput/ColorInput.tsx
@@ -0,0 +1,25 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+import { InputHTMLAttributes } from "react";
+import styles from "./styles.module.scss";
+
+interface TInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  register: UseFormRegisterReturn<string>;
+  className?: string;
+}
+
+export default function ColorInput({
+  register,
+  className,
+  ...attributes
+}: TInputProps) {
+  return (
+    <input
+      type="color"
+      className={`border border-slate-300 dark:border-dark-xl-gray ${
+        styles.colorInput
+      } ${className ? className : ""}`}
+      {...register}
+      {...attributes}
+    />
+  );
+}
