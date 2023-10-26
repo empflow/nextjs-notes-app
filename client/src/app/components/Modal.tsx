8fb2096ffbe35f738d6f53ad2285c906173ca2aa
Matvey <empflow@tutanota.com>
2023-10-26 19:47:44 +0300

    feat(client): make <Modal /> accept any div attr & overlayStyle prop

--- client/src/app/components/Modal.tsx
+++ client/src/app/components/Modal.tsx
@@ -1,35 +1,37 @@
 import { SetState } from "@/utils/types";
-import { MouseEvent, ReactNode } from "react";
+import { CSSProperties, HTMLAttributes, ReactNode } from "react";
 import ReactDom from "React-dom";
-import useIsScreenWidthOverBreakpoint from "../hooks/useIsScreenWidthOverBreakpoint";
 import useQuerySelector from "../hooks/useQuerySelector";
 import Overlay from "./Overlay";
 
-interface TProps {
+interface TProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   isOpen: boolean;
   setIsOpen: SetState<boolean>;
+  overlayStyle?: CSSProperties;
 }
 
-export default function Modal({ children, isOpen, setIsOpen }: TProps) {
-  const isMobile = !useIsScreenWidthOverBreakpoint("md");
+export default function Modal({
+  children,
+  isOpen,
+  setIsOpen,
+  overlayStyle,
+  ...attrs
+}: TProps) {
   const portalContainer = useQuerySelector<HTMLDivElement>("#modals");
   if (!portalContainer) return null;
 
-  function onClick(e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) {
-    e.stopPropagation();
-  }
-
   return ReactDom.createPortal(
     <Overlay
+      style={overlayStyle}
       centerChildren={true}
       isActive={isOpen}
       setIsActive={setIsOpen}
       blurred={true}
     >
       <div
-        onClick={onClick}
-        className="w-full max-w-[600px] rounded border border-light-2xl-gray bg-light-5xl-gray p-2 dark:border-dark-3xl-gray dark:bg-dark-5xl-gray md:p-4"
+        className="w-full max-w-[600px] rounded border border-light-2xl-gray bg-light-5xl-gray p-4 dark:border-dark-3xl-gray dark:bg-dark-5xl-gray md:p-6"
+        {...attrs}
       >
         {children}
       </div>
