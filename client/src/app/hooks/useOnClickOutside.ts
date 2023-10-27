80f642c096aaa42e6d5c9694a4ef6fa4fd8a583b
Matvey <empflow@tutanota.com>
2023-10-27 16:56:50 +0300

    feat(client): add useOnClickOutside.ts

---
+++ client/src/app/hooks/useOnClickOutside.ts
@@ -0,0 +1,21 @@
+import { Ref, RefObject, useRef } from "react";
+import useOnClick from "./useOnClick";
+
+type TEvent = TouchEvent | MouseEvent;
+
+export default function useOnClickOutside<T>(
+  ref: RefObject<HTMLElement>,
+  cb: () => unknown,
+) {
+  useOnClick((e: TEvent) => {
+    console.log("hello");
+    const { target } = e;
+    if (
+      ref?.current &&
+      target instanceof Node &&
+      !ref.current.contains(target)
+    ) {
+      cb();
+    }
+  });
+}
