8d0dc24201b2f568b1ac725e609a2dc3388cc930
Matvey <empflow@tutanota.com>
2023-10-27 16:57:23 +0300

    feat(client): let <Popover /> receive ref prop

--- client/src/app/components/Popover.tsx
+++ client/src/app/components/Popover.tsx
@@ -1,5 +1,5 @@
 import { SetState } from "@/utils/types";
-import { CSSProperties, ReactNode } from "react";
+import { CSSProperties, forwardRef, ReactNode } from "react";
 import Overlay from "./Overlay";
 
 type TProps = {
@@ -32,54 +32,62 @@ interface TPosition {
   right?: string | number;
 }
 
-export default function Popover({
-  children,
-  style,
-  className,
-  position,
-  isOpen,
-  setIsOpen,
-  offset = 0,
-  portalSelector,
-  usePortal = true,
-  overlayStyle,
-  noOverlay,
-}: TProps) {
-  const positions: Record<typeof position, TPosition> = {
-    "top-center": { translate: "-50%", left: "50%", bottom: offset },
-    "top-left": { bottom: offset, right: 0 },
-    "top-right": { bottom: offset, left: 0 },
-    "bottom-center": { translate: "-50%", left: "50%", top: offset },
-    "bottom-left": { top: offset, right: 0 },
-    "bottom-right": { top: offset, left: 0 },
-    left: { translate: "0 50%", bottom: "50%", right: offset ?? 0 },
-    right: { translate: "0 50%", bottom: "50%", left: offset ?? 0 },
-  };
-  const activePosition = positions[position];
+const Popover = forwardRef<HTMLDivElement, TProps>(
+  (
+    {
+      children,
+      style,
+      className,
+      position,
+      isOpen,
+      setIsOpen,
+      offset = 0,
+      portalSelector,
+      usePortal = true,
+      overlayStyle,
+      noOverlay,
+    },
+    ref,
+  ) => {
+    const positions: Record<typeof position, TPosition> = {
+      "top-center": { translate: "-50%", left: "50%", bottom: offset },
+      "top-left": { bottom: offset, right: 0 },
+      "top-right": { bottom: offset, left: 0 },
+      "bottom-center": { translate: "-50%", left: "50%", top: offset },
+      "bottom-left": { top: offset, right: 0 },
+      "bottom-right": { top: offset, left: 0 },
+      left: { translate: "0 50%", bottom: "50%", right: offset ?? 0 },
+      right: { translate: "0 50%", bottom: "50%", left: offset ?? 0 },
+    };
+    const activePosition = positions[position];
 
-  return (
-    <>
-      <div
-        style={{ ...activePosition, ...style }}
-        className={`absolute z-30 duration-100 ${
-          isOpen
-            ? "pointer-events-auto translate-y-0 opacity-100"
-            : "pointer-events-none -translate-y-1 opacity-0"
-        } ${className ?? ""}`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
-      </div>
-      {!noOverlay && (
-        <Overlay
-          style={overlayStyle}
-          usePortal={usePortal}
-          portalSelector={portalSelector}
-          isActive={isOpen}
-          setIsActive={setIsOpen}
-          transparent={true}
-        />
-      )}
-    </>
-  );
-}
+    return (
+      <>
+        <div
+          ref={ref}
+          style={{ ...activePosition, ...style }}
+          className={`absolute z-30 duration-100 ${
+            isOpen
+              ? "pointer-events-auto translate-y-0 opacity-100"
+              : "pointer-events-none -translate-y-1 opacity-0"
+          } ${className ?? ""}`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+        {!noOverlay && (
+          <Overlay
+            style={overlayStyle}
+            usePortal={usePortal}
+            portalSelector={portalSelector}
+            isActive={isOpen}
+            setIsActive={setIsOpen}
+            transparent={true}
+          />
+        )}
+      </>
+    );
+  },
+);
+
+export default Popover;
