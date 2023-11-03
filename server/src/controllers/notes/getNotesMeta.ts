1adb0bd79274c5905e82cca4358656a24b8b335c
Matvey <empflow@tutanota.com>
2023-11-03 09:18:14 +0300

    refactor(server): getNotesMetaCheckQueryParams

--- server/src/controllers/notes/getNotesMeta.ts
+++ server/src/controllers/notes/getNotesMeta.ts
@@ -2,17 +2,12 @@ import { Request, Response } from "express";
 import Note from "../../models/Note";
 import noteMetaProjection from "../../utils/projections/noteMeta";
 
-export default async function getNotesMeta(req: Request, res: Response) {
-  const {
-    query: { tag },
-  } = res.locals;
+export default async function getNotesMeta(_req: Request, res: Response) {
+  const { query } = res.locals;
   const userId: string = res.locals.jwtPayload.userId;
 
-  const tagQuery: Partial<{ tags: string }> = {};
-  if (tag) tagQuery.tags = tag;
-
   const notes = await Note.find(
-    { owner: userId, ...tagQuery },
+    { owner: userId, ...query },
     noteMetaProjection
   );
   res.status(200).json(notes);
--- server/src/middleware/notes/getNotesMeta/checkQueryParams.ts
+++ server/src/middleware/notes/getNotesMeta/checkQueryParams.ts
@@ -1,5 +1,7 @@
+import { TNoteServer } from "@/server/src/models/Note";
 import { BadRequestErr } from "@/server/src/utils/errs";
 import { Request, Response, NextFunction } from "express";
+import { FilterQuery } from "mongoose";
 
 export default async function getNotesMetaCheckQueryParams(
   req: Request,
@@ -7,14 +9,15 @@ export default async function getNotesMetaCheckQueryParams(
   next: NextFunction
 ) {
   const { tag } = req.query;
+  const query: FilterQuery<TNoteServer> = {};
 
   if (tag) {
     if (typeof tag !== "string") {
       throw new BadRequestErr("`tag` query parameter is not of type string");
     }
+    query.tags = tag;
   }
 
-  res.locals.query = { tag };
-
+  res.locals.query = query;
   next();
 }
