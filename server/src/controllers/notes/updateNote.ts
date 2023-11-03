a839ca0fb516227c9afb6b1acecb3700b14d628e
Matvey <empflow@tutanota.com>
2023-11-03 09:26:28 +0300

    fix(server): remove ability to assign same tags twice or more to same note

--- server/src/controllers/notes/updateNote.ts
+++ server/src/controllers/notes/updateNote.ts
@@ -7,10 +7,11 @@ export default async function updateNote(req: Request, res: Response) {
   const userId = res.locals.jwtPayload.userId;
   const { isInTrash, tags, content } = req.body;
   const { description, title } = res.locals;
+  const uniqueTags = tags ? [...new Set(tags)] : undefined;
 
   const noteUpdateResult = await Note.updateOne(
     { owner: userId, _id: noteId },
-    { title, content, isInTrash, tags, description },
+    { title, content, isInTrash, tags: uniqueTags, description },
     { new: true, runValidators: true }
   );
   if (!noteUpdateResult.modifiedCount) throw new NotFoundErr("Note not found");
