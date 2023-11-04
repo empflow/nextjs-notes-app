612b702088a20a8f23d7577ab39cd88d9baac381
Matvey <empflow@tutanota.com>
2023-11-04 12:41:35 +0300

    feat(client): add notes with a default tag
    if a note is created while the notes list is filtering notes by a specific tag, the note note is assigned that tag

--- client/src/app/hooks/reactQuery/useAddNoteMutation.ts
+++ client/src/app/hooks/reactQuery/useAddNoteMutation.ts
@@ -2,13 +2,17 @@ import NotesContext from "@/contexts/NotesContext";
 import httpWithAuth from "@/utils/http/httpWithAuth/httpWithAuth";
 import notify from "@/utils/notify";
 import { noteSchema, TNoteMetaSchema, TNoteSchema } from "@shared/schemas/note";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  MutationFunction,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { useTranslations } from "next-intl";
 import useGetContext from "../useGetContext";
 import { convertNoteToNoteMeta } from "../../../utils/convertNoteToNoteMeta";
 
-  const { setSelectedNoteId } = useGetContext(NotesContext);
 export default function useAddNoteMutation() {
+  const { setSelectedNoteId, selectedTagId } = useGetContext(NotesContext);
   const errsT = useTranslations("Errors");
   const queryClient = useQueryClient();
 
@@ -29,9 +33,14 @@ export default function useAddNoteMutation() {
       queryClient.invalidateQueries(["notes"]);
     },
   });
-}
 
-async function addNote() {
-  const { data } = await httpWithAuth.post("/notes/add");
-  return noteSchema.parse(data);
+  async function addNote() {
+    let payload: undefined | Partial<TNoteSchema> = {};
+    if (selectedTagId) payload.tags = [selectedTagId];
+
+    const { data } = await httpWithAuth.post("/notes/add", {
+      tags: selectedTagId ? [selectedTagId] : undefined,
+    });
+    return noteSchema.parse(data);
+  }
 }
